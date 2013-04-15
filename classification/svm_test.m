08286c8f3604a1426f827af52b102c160cde6c1e
Joakim Andén <joakim.anden@gmail.com>
2013-04-15 15:02:34 +0200

    add svm_train and svm_test

---
+++ classification/svm_test.m
@@ -0,0 +1,135 @@
+function [labels,votes,feature_labels,K,sv_coef,dec] = svm_test(db,model,prt_test)
+	ind_features = [db.indices{prt_test}];
+	
+	if ~model.full_test_kernel && ...
+		(model.svm.Parameters(2) == 4 || ...
+		model.svm.Parameters(2) == 5 || ...
+		model.svm.Parameters(2) == 6 || ...
+		model.svm.Parameters(2) == 7)
+		[kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
+	
+		ind_features = ind_features(kernel_mask(ind_features));
+	end
+
+	[feature_labels,temp,K,sv_coef,dec] = svm_feature_test(db,model.svm,ind_features,model.full_test_kernel);
+		
+	for l = 1:length(prt_test)
+		ind = find(ismember(ind_features,db.indices{prt_test(l)}));
+		
+		votes(:,l) = histc(feature_labels(ind),1:length(db.src.classes));
+		
+		[temp,labels(l)] = max(votes(:,l));
+	end
+end
+
+function [labels,votes,K,sv_coef,dec] = svm_feature_test(db,model,ind_features0,full_test_kernel)
+	block_size = 8192;
+
+	class_ct = model.nr_class;
+	
+	sv_coef = zeros(model.totalSV,class_ct*(class_ct-1)/2,class(db.kernel.K));
+	
+	pairs = [];
+	
+	r = 1;
+	for n1 = 1:class_ct
+		for n2 = n1+1:class_ct
+			pairs = [pairs [n1; n2]];
+			
+			class1_SVs = 1+sum(model.nSV(1:n1-1)):sum(model.nSV(1:n1));
+			class2_SVs = 1+sum(model.nSV(1:n2-1)):sum(model.nSV(1:n2));
+			
+			sv_coef(class1_SVs,r) = model.sv_coef(class1_SVs,n2-1);
+			sv_coef(class2_SVs,r) = model.sv_coef(class2_SVs,n1);
+			r = r+1;
+		end
+	end
+
+	dec = zeros(length(ind_features0),size(sv_coef,2),class(db.kernel.K));
+
+	if full_test_kernel && ...
+		(model.Parameters(2) == 4 || ...
+		 model.Parameters(2) == 5 || ...
+		 model.Parameters(2) == 6 || ...
+		 model.Parameters(2) == 7)
+		norm2 = sum(abs(db.features(:,db.kernel.kernel_set(model.SVs))).^2,1);
+	end
+
+	n = 1;
+	while n <= length(ind_features0)
+		mask = n:min(n+block_size-1,length(ind_features0));
+		ind_features = ind_features0(mask);
+		
+		if model.Parameters(2) == 0			% linear kernel
+			K = db.features(:,ind_features).'*model.SVs.';
+		elseif model.Parameters(2) == 2		% Gaussian kernel
+			norm1 = sum(abs(db.features(:,ind_features)).^2,1);
+			norm2 = sum(abs(model.SVs.').^2,1);
+			K = bsxfun(@plus,norm1.',norm2)-... 
+				2*db.features(:,ind_features).'*model.SVs.';
+			K = exp(-model.Parameters(4)*K);
+		elseif full_test_kernel && ...
+			(model.Parameters(2) == 4 || ...
+			 model.Parameters(2) == 5 || ...
+			 model.Parameters(2) == 6 || ...
+			 model.Parameters(2) == 7)
+		
+			if strcmp(db.kernel.kernel_type,'linear')
+				K = db.features(:,ind_features).'*db.features(:,db.kernel.kernel_set(model.SVs));
+			elseif strcmp(db.kernel.kernel_type,'gaussian')
+				norm1 = sum(abs(db.features(:,ind_features)).^2,1);
+				%norm2 = sum(abs(db.features(:,db.kernel.kernel_set(model.SVs))).^2,1);
+				K = bsxfun(@plus,norm1.',norm2)-... 
+					2*db.features(:,ind_features).'*db.features(:,db.kernel.kernel_set(model.SVs));
+			else
+				error('unknown kernel type');
+			end
+		elseif model.Parameters(2) == 4	|| ...
+			   model.Parameters(2) == 6 % Precalculated kernel (in db.kernel)
+			[kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
+			K = db.kernel.K(1+model.SVs,kernel_ind(ind_features)).';
+		elseif model.Parameters(2) == 5 || ...
+			   model.Parameters(2) == 7
+			K = zeros(length(ind_features),length(model.SVs),class(db.kernel.K));
+			
+			[kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
+		
+			for k = 1:length(ind_features)
+				idx = kernel_ind(ind_features(k));
+				
+				% Need to cast to integers since floating-point indexing causes
+				% problems. Take 64 bits to be safe...
+				lower_SVs = int64(model.SVs(model.SVs<=idx));
+				upper_SVs = int64(model.SVs(model.SVs>idx));
+				
+				K(k,model.SVs<=idx) = ...
+					db.kernel.K((idx-1)*(idx-1+3)/2+1+lower_SVs);
+				K(k,model.SVs>idx) = ...
+					db.kernel.K((upper_SVs-1).*(upper_SVs-1+3)/2+1+idx);
+			end
+		end
+		
+		if (model.Parameters(2) == 4 && strcmp(db.kernel.kernel_type,'gaussian')) || ...
+		   model.Parameters(2) == 6 || model.Parameters(2) == 7
+			K = exp(-model.Parameters(4)*K);
+		end
+
+		dec(mask,:) = bsxfun(@minus,K*sv_coef,model.rho.');
+	
+		n = n+length(mask);
+	end
+
+	votes = zeros(size(dec,1),class_ct);
+	
+	for r = 1:size(pairs,2)
+		n1 = pairs(1,r);
+		n2 = pairs(2,r);
+		
+		votes(dec(:,r)>=0,n1) = votes(dec(:,r)>=0,n1)+1;
+		votes(dec(:,r)<0,n2) = votes(dec(:,r)<0,n2)+1;
+	end
+	
+	[temp,labels] = max(votes,[],2);
+	
+	labels = model.Label(labels);
+end
---
+++ classification/svm_train.m
@@ -0,0 +1,92 @@
+function model = svm_train(db,prt_train,opt)
+	if nargin < 3
+		opt = struct();
+	end
+	
+	%if ~exist('svmtrain_inplace')
+	%	error('you should make sure svmtrain_inplace is available...');
+	%end
+
+	opt = fill_struct(opt,'no_inplace',0);
+	opt = fill_struct(opt,'full_test_kernel',0);
+
+	opt = fill_struct(opt,'kernel_type','gaussian');
+	
+	opt = fill_struct(opt,'gamma',1e-4);
+	opt = fill_struct(opt,'C',8);
+	
+	ind_features = [];
+	feature_class = [];
+	for k = 1:length(prt_train)
+		ind = db.indices{prt_train(k)};
+		ind_features = [ind_features ind];
+		feature_class = [feature_class ...
+			db.src.objects(prt_train(k)).class*ones(1,length(ind))];
+	end
+
+	
+	precalc_kernel = isfield(db,'kernel') && ...
+		strcmp(opt.kernel_type,db.kernel.kernel_type);
+	
+	params = ['-q -c ' num2str(opt.C)];
+	if ~precalc_kernel
+		if strcmp(opt.kernel_type,'linear')
+			params = [params ' -t 0'];
+		elseif strcmp(opt.kernel_type,'gaussian')
+			params = [params ' -t 2 -g ' num2str(opt.gamma)];
+		else
+			error('Unknown kernel type!');
+		end
+		features = db.features(:,ind_features);
+	else
+		[kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
+
+		ind_features = kernel_ind(ind_features(kernel_mask(ind_features)));
+
+		if exist('svmtrain_inplace') && ~opt.no_inplace
+			feature_class = zeros(1,size(db.features,2));
+			for k = 1:length(db.indices)
+				feature_class(db.indices{k}) = db.src.objects(k).class;
+			end
+			feature_class = feature_class(db.kernel.kernel_set);
+			features = db.kernel.K;
+
+			if strcmp(db.kernel.kernel_type,'linear') && ...
+				strcmp(db.kernel.kernel_format,'square')
+				params = [params ' -t 4'];
+			elseif strcmp(db.kernel.kernel_type,'linear') && ...
+				strcmp(db.kernel.kernel_format,'triangle')
+				params = [params ' -t 5'];
+			elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
+				strcmp(db.kernel.kernel_format,'square')
+				params = [params ' -t 6 -g ' num2str(opt.gamma)];
+			elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
+				strcmp(db.kernel.kernel_format,'triangle')
+				params = [params ' -t 7 -g ' num2str(opt.gamma)];
+			else
+				error('Unknown kernel type/format!');
+			end
+		else
+			params = [params ' -t 4'];
+			
+			features = db.kernel.K(:,ind_features);
+			
+			if strcmp(db.kernel.kernel_type,'gaussian')
+				features(2:end,:) = exp(-opt.gamma*features(2:end,:));
+				params = [params ' -g ' num2str(opt.gamma)];
+			end
+		end
+	end
+
+	model.full_test_kernel = opt.full_test_kernel;
+
+	model.prt_train = prt_train;
+	
+	if ~exist('svmtrain_inplace') || opt.no_inplace
+		model.svm = svmtrain(feature_class.', ...
+			double(features.'),params);
+	else
+		model.svm = svmtrain_inplace(feature_class, ...
+			single(features),params,ind_features);
+	end
+end
