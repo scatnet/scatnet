7aa08e2dc6f49d6da7a0c05cd5893caae5deed77
Anden Joakim <joakim.anden@gmail.com>
2013-04-16 18:20:23 +0200

    add gtzan_affine and gtzan_svm tests

---
+++ test/classification/test_gtzan_affine.m
@@ -0,0 +1,26 @@
+src = gtzan_src('~/matlab/y/gtzan');
+
+N = 5*2^17;
+
+filt1_opt.V = 8;
+filt1_opt.J = 80;
+filt1_opt.gabor = 1;
+
+filt2_opt.V = 1;
+filt2_opt.J = 13;
+
+filters1 = morlet_filter_bank_1d(N,filt1_opt);
+filters2 = morlet_filter_bank_1d(N,filt2_opt);
+
+scatt_fun = @(x)(scatt_1d(x,{@(x)(wavemod_1d(x,filters1)),@(x)(wavemod_1d(x,filters2)),@(x)(wavemod_1d(x,filters2))}));
+scatt_fun = @(x)(format_scatt(log_scatt(renorm_scatt(scatt_fun(x))),'table'));
+
+db = prepare_db(src,{scatt_fun});
+
+[prt_train,prt_test] = create_partition(src);
+
+model = affine_train(db,prt_train);
+labels = affine_test(db,model,prt_test);
+
+err = classif_err(labels,prt_test,src);
+
---
+++ test/classification/test_gtzan_svm.m
@@ -0,0 +1,32 @@
+src = gtzan_src('~/matlab/y/gtzan');
+
+N = 5*2^17;
+
+filt1_opt.V = 8;
+filt1_opt.J = 80;
+filt1_opt.gabor = 1;
+
+filt2_opt.V = 1;
+filt2_opt.J = 13;
+
+filters1 = morlet_filter_bank_1d(N,filt1_opt);
+filters2 = morlet_filter_bank_1d(N,filt2_opt);
+
+scatt_fun = @(x)(scatt_1d(x,{@(x)(wavemod_1d(x,filters1)),@(x)(wavemod_1d(x,filters2)),@(x)(wavemod_1d(x,filters2))}));
+scatt_fun = @(x)(format_scatt(log_scatt(renorm_scatt(scatt_fun(x))),'table'));
+
+db = prepare_db(src,{scatt_fun},struct('feature_sampling',8));
+
+[prt_train,prt_test] = create_partition(src);
+
+optt.kernel_type = 'gaussian';
+optt.C = 2^4;
+optt.gamma = 2^(-12);
+
+addpath('~/matlab/libsvm-3.1/matlab');
+
+model = svm_train(db,prt_train,optt);
+labels = svm_test(db,model,prt_test);
+
+err = classif_err(labels,prt_test,src);
+
