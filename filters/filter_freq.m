7e94c66725d1ff1871f353cb6737f03aff02ad14
Joakim Andén <joakim.anden@gmail.com>
2013-04-16 16:49:36 +0200

    fix filter_freq bug - needs to recognize gabor_1d filter type

--- filters/filter_freq.m
+++ filters/filter_freq.m
@@ -1,7 +1,8 @@
 function [psi_xi,psi_bw,phi_bw] = filter_freq(filters)
 	if strcmp(filters.filter_type,'spline_1d')
 		[psi_xi,psi_bw,phi_bw] = spline_1d_freq(filters);
-	elseif strcmp(filters.filter_type,'morlet_1d')
+	elseif strcmp(filters.filter_type,'morlet_1d') || ...
+		strcmp(filters.filter_type,'gabor_1d')
 		[psi_xi,psi_bw,phi_bw] = morlet_1d_freq(filters);
 	else
 		error(sprintf('Unknown filter type ''%s''',filters.filter_type))
