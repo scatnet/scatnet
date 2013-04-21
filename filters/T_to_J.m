ee967d045f533bfeb7557f1560049617c4437a46
Joakim Andén <joakim.anden@gmail.com>
2013-04-21 19:31:20 +0200

    add T_to_J (need to verify that it is correct!)

---
+++ filters/T_to_J.m
@@ -0,0 +1,21 @@
+%TODO redo doc
+%T_TO_J Calculates the maximal wavelet scale for a filter bank
+%   J = T_to_J(T,Q,a) calculates the closest integer J corresponding to T
+%   gives Q, a such that T = Q*a^J. By default a = 2^(1/Q) if Q is specified,
+%   otherwise Q = 1, a = 2.
+
+function J = T_to_J(T, Q, B, phi_bw_multiplier)
+	if nargin < 2 || isempty(Q)
+		Q = 1;
+	end
+	
+	if nargin < 3 || isempty(B)
+		B = Q;
+	end
+	
+	if nargin < 4 || isempty(phi_bw_multiplier)
+		phi_bw_multiplier = 1+(Q==1);
+	end
+	
+	J = round(log2(T./(B./phi_bw_multiplier)).*Q);
+end
