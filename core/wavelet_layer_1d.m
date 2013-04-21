ea55d55d3163940fd2d7f2b96d0d9571d213e333
Joakim Andén <joakim.anden@gmail.com>
2013-04-21 17:04:16 +0200

    fix wavelet_layer_1d to be compatible with scatt

--- core/wavelet_layer_1d.m
+++ core/wavelet_layer_1d.m
@@ -19,15 +19,18 @@ function [U_phi, U_psi] = wavelet_layer_1d(U, filters, options)
 
 	[psi_xi,psi_bw,phi_bw] = filter_freq(filters);
 	
+	if ~isfield(U.meta, 'bandwidth'), U.meta.bandwidth = 2*pi; end
+	if ~isfield(U.meta, 'resolution'), U.meta.resolution = 0; end
+	
 	U_phi.signal = {};
 	U_phi.meta.bandwidth = [];
 	U_phi.meta.resolution = [];
-	U_phi.meta.scale = zeros(0,size(U.meta.scale,2));
+	U_phi.meta.j = zeros(0,size(U.meta.j,2));
 	
 	U_psi.signal = {};
 	U_psi.meta.bandwidth = [];
 	U_psi.meta.resolution = [];
-	U_psi.meta.scale = zeros(0,size(U.meta.scale,2)+1);
+	U_psi.meta.j = zeros(0,size(U.meta.j,2)+1);
 	
 	r = 1;
 	for p1 = 1:length(U.signal)
@@ -38,13 +41,13 @@ function [U_phi, U_psi] = wavelet_layer_1d(U, filters, options)
 		U_phi.signal{p1} = x_phi;
 		U_phi.meta.bandwidth(p1,1) = phi_bw;
 		U_phi.meta.resolution(p1,1) = log2(filters.N/size(x_phi,1));
-		U_phi.meta.scale(p1,:) = U.meta.scale(p1,:);
+		U_phi.meta.j(p1,:) = U.meta.j(p1,:);
 		
 		for p2 = find(~cellfun(@isempty,x_psi))
 			U_psi.signal{r} = x_psi{p2};
 			U_psi.meta.bandwidth(r,1) = psi_bw(p2);
 			U_psi.meta.resolution(r,1) = log2(filters.N/size(x_psi{p2},1));
-			U_psi.meta.scale(r,:) = [U.meta.scale(p1,:) p2-1];
+			U_psi.meta.j(r,:) = [U.meta.j(p1,:) p2-1];
 			
 			r = r+1;
 		end
