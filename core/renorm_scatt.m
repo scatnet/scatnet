c94599ac36b0e99a3df1d9f3c5596aa5ec112de3
Anden Joakim <joakim.anden@gmail.com>
2013-04-23 00:20:47 +0200

    fix renorm_scatt bug caused by scale->j and dimension change

--- core/renorm_scatt.m
+++ core/renorm_scatt.m
@@ -18,8 +18,8 @@ function X = renorm_scatt(X,epsilon,min_order)
 	
 	for m = length(X)-1:-1:min_order
 		for p2 = 1:length(X{m+1}.signal)
-			scale = X{m+1}.meta.scale(p2,:);
-			p1 = find(all(bsxfun(@eq,X{m}.meta.scale,scale(1:m-1)),2));
+			j = X{m+1}.meta.j(:,p2);
+			p1 = find(all(bsxfun(@eq,X{m}.meta.j,j(1:m-1)),1));
 			sub_multiplier = 2^(X{m}.meta.resolution(p1)/2);
 			X{m+1}.signal{p2} = X{m+1}.signal{p2}./(X{m}.signal{p1}+epsilon*sub_multiplier);
 		end
