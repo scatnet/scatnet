6f8b3728002a5d54533174d6f6870ceda54132b7
Joakim Andén <joakim.anden@gmail.com>
2013-04-22 16:15:34 +0200

    make first dimension time/space, last dimension path

--- core/wavelet_layer_1d.m
+++ core/wavelet_layer_1d.m
@@ -25,12 +25,12 @@ function [U_phi, U_psi] = wavelet_layer_1d(U, filters, options)
 	U_phi.signal = {};
 	U_phi.meta.bandwidth = [];
 	U_phi.meta.resolution = [];
-	U_phi.meta.j = zeros(0,size(U.meta.j,2));
+	U_phi.meta.j = zeros(size(U.meta.j,1),0);
 	
 	U_psi.signal = {};
 	U_psi.meta.bandwidth = [];
 	U_psi.meta.resolution = [];
-	U_psi.meta.j = zeros(0,size(U.meta.j,2)+1);
+	U_psi.meta.j = zeros(size(U.meta.j,1)+1,0);
 	
 	r = 1;
 	for p1 = 1:length(U.signal)
@@ -38,16 +38,16 @@ function [U_phi, U_psi] = wavelet_layer_1d(U, filters, options)
 		
 		[x_phi, x_psi] = wavelet_1d(U.signal{p1}, filters, options);
 		
-		U_phi.signal{p1} = x_phi;
-		U_phi.meta.bandwidth(p1,1) = phi_bw;
-		U_phi.meta.resolution(p1,1) = log2(filters.N/size(x_phi,1));
-		U_phi.meta.j(p1,:) = U.meta.j(p1,:);
+		U_phi.signal{1,p1} = x_phi;
+		U_phi.meta.bandwidth(1,p1) = phi_bw;
+		U_phi.meta.resolution(1,p1) = log2(filters.N/size(x_phi,1));
+		U_phi.meta.j(:,p1) = U.meta.j(:,p1);
 		
 		for p2 = find(~cellfun(@isempty,x_psi))
-			U_psi.signal{r} = x_psi{p2};
-			U_psi.meta.bandwidth(r,1) = psi_bw(p2);
-			U_psi.meta.resolution(r,1) = log2(filters.N/size(x_psi{p2},1));
-			U_psi.meta.j(r,:) = [U.meta.j(p1,:) p2-1];
+			U_psi.signal{1,r} = x_psi{p2};
+			U_psi.meta.bandwidth(1,r) = psi_bw(p2);
+			U_psi.meta.resolution(1,r) = log2(filters.N/size(x_psi{p2},1));
+			U_psi.meta.j(:,r) = [U.meta.j(:,p1); p2-1];
 			
 			r = r+1;
 		end
