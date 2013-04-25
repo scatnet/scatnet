1e5beb1e33cea23ab71b0976c5965428fc345465
Anden Joakim <joakim.anden@gmail.com>
2013-04-25 12:06:44 +0200

    fix phone_partitions transition bug

--- classification/phone_partitions.m
+++ classification/phone_partitions.m
@@ -1,5 +1,5 @@
 function [prt_train,prt_test,prt_dev] = phone_partition(src)
-	prt_train = find([src.segments.subset]==0);
-	prt_test = find([src.segments.subset]==1);
-	prt_dev = find([src.segments.subset]==2);
+	prt_train = find([src.objects.subset]==0);
+	prt_test = find([src.objects.subset]==1);
+	prt_dev = find([src.objects.subset]==2);
 end
