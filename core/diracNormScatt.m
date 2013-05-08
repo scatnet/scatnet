c3e9e15fe6bb5cd2337c98769dc3d1262421acf8
Michel Kapoko <michelkapoko@gmail.com>
2013-05-09 00:07:03 +0200

    integrate the code for the "normalized scattering representation" defined by Stephane in his paper "Group Invariant Scattering"

---
+++ core/diracNormScatt.m
@@ -0,0 +1,50 @@
+function nScatt = normScatt(x,cascade)
+% Calculation of a normalized scattering representation
+% nScatt is an array of size 2N which stores a
+% piecewise constant signal giving
+% the 2^J normalized values || SJ [p]f||/||SJ[p] delta|| 
+% over intervals of lengths proportional to ||SJ[p] delta||^2 .
+% Normalized scattering transforms are defined in the paper
+% "Group Invariant Scattering", S. Mallat, http://arxiv.org/abs/1101.2286
+
+N=size(x,1); % N must be an integer multiplied by a very high power of 2
+%Compute the scattering transform of the signal x and output it in a table
+Sx=scatt(x,cascade);
+
+%Prepare the dirac signal
+   dirac = zeros(N,1);
+   dirac(N/2) = 1;
+%Compute its scattering transform and output it in the form of a table
+Sdirac=scatt(dirac, cascade);
+
+[Scatt order2]=reorderScatt(Sx);
+   nSdirac=reorderScatt(Sdirac);
+   
+   lsig=2*N/2^(length(Sx{2}.signal));
+
+  k=1;
+    while k<2*N
+          normd=norm(nSdirac(k:k+lsig-1));
+          normx=norm(Scatt(1,k:k+lsig-1))/normd;
+          normx = 10^(-6) * floor(normx * 10^(6));
+          
+          Scatt(k:k+lsig-1) = normx + order2(k)* 10^(-7);
+          nSdirac(k:k+lsig-1)=normd^2;
+           
+         k=k+lsig;
+    end
+        
+        C=sum(nSdirac)/lsig;
+    
+    p=0;
+    for k=1:lsig:2*N
+    step=nSdirac(k) * 2 * N/C;
+    nScatt(round(p+1):round(p+step))=Scatt(k);
+    p=p+step;
+    end
+    
+      
+
+end
+
+
