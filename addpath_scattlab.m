8df6f6de011b2d0394049dc8b08310f2018ea494
Michel Kapoko <michelkapoko@gmail.com>
2013-05-09 16:54:36 +0200

    Added the path to the demo file

--- addpath_scattlab.m
+++ addpath_scattlab.m
@@ -7,6 +7,8 @@ addpath(fullfile(path_to_scattlab, 'display'));
 addpath(fullfile(path_to_scattlab, 'filters'));
 addpath(fullfile(path_to_scattlab, 'utils'));
 addpath(fullfile(path_to_scattlab, 'wrappers'));
+addpath(fullfile(path_to_scattlab, 'demo'));
+addpath(fullfile(path_to_scattlab, 'demo/GIS'));
 
 clear path_to_scattlab;
 
