caeaaa12ad540182368f6b6ae57664ea5edad704
Joakim Andén <joakim.anden@gmail.com>
2013-05-09 14:22:15 +0200

    make separable_wavelet_2d pad/unpad itself

--- core/separable_cascade_factory_2d.m
+++ core/separable_cascade_factory_2d.m
@@ -1,15 +1,15 @@
-function cascade = separable_cascade_factory_2d(N, filter_options, scatt_options, M)
+function cascade = separable_cascade_factory_2d(N, filter_options, scat_options, M)
 	filters{1} = filter_bank(N(1),filter_options{1});
 	filters{2} = filter_bank(N(2),filter_options{2});
 	
-	cascade.pad = @(X)(pad_layer_1d(X, 2*N, 'symm'));
-	cascade.unpad = @(X)(unpad_layer_1d(X, N));
+	%cascade.pad = @(X)(pad_layer_1d(X, 2*N, 'symm'));
+	%cascade.unpad = @(X)(unpad_layer_1d(X, N));
 	
 	for m = 0:M
 		filt1_ind = min(numel(filters{1}), m+1);
 		filt2_ind = min(numel(filters{2}), m+1);
-		cascade.wavelet{m+1} = @(X)(separable_wavelet_layer_2d(X, ...
-			{filters{1}{filt1_ind} filters{2}{filt2_ind}}, scatt_options));
-		cascade.modulus{m+1} = @modulus_layer;
+		cascade{m+1} = @(X)(separable_wavelet_layer_2d(X, ...
+			{filters{1}{filt1_ind} filters{2}{filt2_ind}}, scat_options));
+		%cascade.modulus{m+1} = @modulus_layer;
 	end
 end
