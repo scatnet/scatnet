64df3e5c5e1c0db8cbe274598814aa9250df7ba9
Joakim Andén <joakim.anden@gmail.com>
2013-05-09 12:00:41 +0200

    make wavelet_1d output metas, otherwise we can no longer compute resolution accurately

--- core/wavelet_1d.m
+++ core/wavelet_1d.m
@@ -1,6 +1,6 @@
 % wavelet_1d: 1D wavelet transform.
 % Usage
-%    [x_phi, x_psi] = wavelet_1d(x, filters, options)
+%    [x_phi, x_psi, meta_phi, meta_psi] = wavelet_1d(x, filters, options)
 % Input
 %    x: The signal to be transformed.
 %    filters: The filters of the wavelet transform.
@@ -9,8 +9,9 @@
 % Output
 %    x_phi: x filtered by lowpass filter phi
 %    x_psi: cell array of x filtered by wavelets psi
+%    meta_phi, meta_psi: meta information on x_phi and x_psi, respectively
 
-function [x_phi, x_psi] = wavelet_1d(x, filters, options)
+function [x_phi, x_psi, meta_phi, meta_psi] = wavelet_1d(x, filters, options)
 	if nargin < 3
 		options = struct();
 	end
@@ -39,8 +40,14 @@ function [x_phi, x_psi] = wavelet_1d(x, filters, options)
 	
 	x_phi = real(conv_sub_1d(xf, filters.phi.filter, ds));
 	x_phi = unpad_signal_1d(x_phi, ds, N);
+	meta_phi.j = -1;
+	meta_phi.bandwidth = phi_bw;
+	meta_phi.resolution = ds;
 	
 	x_psi = cell(1, numel(filters.psi.filter));
+	meta_psi.j = -1*ones(1, numel(filters.psi.filter));
+	meta_psi.bandwidth = -1*ones(1, numel(filters.psi.filter));
+	meta_psi.resolution = -1*ones(1, numel(filters.psi.filter));
 	for p1 = find(options.psi_mask)
 		ds = round(log2(2*pi/psi_bw(p1)/2)) - ...
 		     j0 - ...
@@ -49,5 +56,8 @@ function [x_phi, x_psi] = wavelet_1d(x, filters, options)
 		
 		x_psi{p1} = conv_sub_1d(xf, filters.psi.filter{p1}, ds);
 		x_psi{p1} = unpad_signal_1d(x_psi{p1}, ds, N);
+		meta_psi.j(:,p1) = p1-1;
+		meta_psi.bandwidth(:,p1) = psi_bw(p1);
+		meta_psi.resolution(:,p1) = ds;
 	end
 end
