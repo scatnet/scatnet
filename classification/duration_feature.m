f0f15f56faab83c71c448adec3bd944ba0cac295
Joakim Andén <joakim.anden@gmail.com>
2013-05-12 21:01:58 +0200

    add doc

--- classification/duration_feature.m
+++ classification/duration_feature.m
@@ -1,3 +1,12 @@
-function t = duration_feature(x,segment)
-	t = permute(log([segment.u2]-[segment.u1]+1),[1 3 2]);
+% duration_feature: Calculate the log-duration of a segment.
+% Usage
+%    duration = duration_feature(x, object)
+% Input
+%    x: The file data (not used).
+%    object: The objects contained in the data.
+% Output
+%    duration: The log-duration of the objects.
+
+function t = duration_feature(x, object)
+	t = permute(log([object.u2]-[object.u1]+1),[1 3 2]);
 end
