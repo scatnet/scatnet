7e027298b4fed66664753a3a68129f5b0d9d7df3
Joakim Andén <joakim.anden@gmail.com>
2013-05-27 22:44:10 +0200

    make joint_tf factory only do renormalize for m>1

--- core/joint_tf_wavelet_factory_1d.m
+++ core/joint_tf_wavelet_factory_1d.m
@@ -5,7 +5,9 @@ function wavelet = joint_tf_wavelet_factory_1d(N, time_filter_options, freq_filt
 	
 	freq_filters = filter_bank(N_freq, freq_filter_options);
 	
-	wavelet{1} = @(X)(wavelet_layer_1d(X, time_filters{1}, scat_options));
+	scat_options1 = scat_options;
+	scat_options1.phi_renormalize = 0;
+	wavelet{1} = @(X)(wavelet_layer_1d(X, time_filters{1}, scat_options1));
 	
 	for m = 1:M
 		time_filt_ind = min(numel(time_filters), m+1);
