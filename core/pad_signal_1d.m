e52fef76b7d6cac155d2afe9254ead51cbab2629
Joakim Andén <joakim.anden@gmail.com>
2013-05-30 18:11:09 +0200

    add zero padding (unused for now)

--- core/pad_signal_1d.m
+++ core/pad_signal_1d.m
@@ -6,16 +6,22 @@ function x = pad_signal_1d(x,N1,boundary)
 			ind0 = [1:N0 N0:-1:1];
 		elseif strcmp(boundary,'per')
 			ind0 = [1:N0];
+		elseif strcmp(boundary,'zero')
+			ind0 = [1:N0];				% UNUSED FOR NOW
 		else
 			error('Invalid boundary conditions!');
 		end
 	
-		ind = zeros(1,N1(d));
-		ind(1:N0) = 1:N0;
-		ind(N0+1:N0+floor((N1(d)-N0)/2)) = ...
-			ind0(mod([N0+1:N0+floor((N1(d)-N0)/2)]-1,length(ind0))+1);
-		ind(N1(d):-1:N0+floor((N1(d)-N0)/2)+1) = ...
-			ind0(mod([1:ceil((N1(d)-N0)/2)]-1,length(ind0))+1);
+		if ~strcmp(boundary,'zero')
+			ind = zeros(1,N1(d));
+			ind(1:N0) = 1:N0;
+			ind(N0+1:N0+floor((N1(d)-N0)/2)) = ...
+				ind0(mod([N0+1:N0+floor((N1(d)-N0)/2)]-1,length(ind0))+1);
+			ind(N1(d):-1:N0+floor((N1(d)-N0)/2)+1) = ...
+				ind0(mod([1:ceil((N1(d)-N0)/2)]-1,length(ind0))+1);
+		else
+			ind = 1:N0;
+		end
 	
 		%x = shiftdim(x,d-1);
 		%sz = size(x);
@@ -27,8 +33,14 @@ function x = pad_signal_1d(x,N1,boundary)
 		% MATLAB is stupid; easier to do manually
 		if d == 1
 			x = x(ind,:,:);
+			if strcmp(boundary,'zero')
+				x(N0+1:N1,:,:) = 0;
+			end
 		elseif d == 2
 			x = x(:,ind,:);
+			if strcmp(boundary,'zero')
+				x(:,N0+1:N1,:) = 0;
+			end
 		end
 	end
 end
