4ae91d72671f58ddac4a03aefa52128154b28042
Joakim Andén <joakim.anden@gmail.com>
2013-06-04 12:08:34 +0200

    scattergram refactoring

--- display/scattergram.m
+++ display/scattergram.m
@@ -1,68 +1,30 @@
-function [] = scattergram(U,S1,layerNb,j1,eps1,eps2,eps3)
-
-%This function returns the set of three tables to be used for the
+% This function returns the set of three tables to be used for the
 % the display of scalograms.
 
-imgU1=scattergram_1(U,layerNb,eps1);
-imgS1=scattergram_1(S1,layerNb,eps2);
+function scattergram(U,S1,layerNb,j1,eps1,eps2,eps3)
+	imgU1 = scat_img(U{layerNb},zeros(0,1),eps1);
+	imgS1 = scat_img(S1{layerNb},zeros(0,1),eps2);
+	
+	S1 = renorm_scat(S1);
 
+	imgS2_j1 = scat_img(S1{layerNb+1},j1,eps3);
 
-imgS2_j1=scattergram_2(S1,layerNb+1,j1,eps3);figure;
-
-subplot(3,1,1);imagesc(imgU1);
-subplot(3,1,2);imagesc(imgS1); 
-subplot(3,1,3);imagesc(imgS2_j1);
+	subplot(3,1,1);imagesc(imgU1);
+	subplot(3,1,2);imagesc(imgS1); 
+	subplot(3,1,3);imagesc(imgS2_j1);
 end
 
-
-
-function img = scattergram_1(U, layerNb, epsilon)
-%dcompute of associated to the layer layerNb. Note that the
-%order of the scattering coefficient is equal to layerNb-1
-
-nbFreqPt=length(U{layerNb}.signal);
-nbTimePt=length(U{layerNb}.signal{1}); % this value may vary with time
-
-img=zeros(nbFreqPt,nbTimePt);
-for k=1:nbFreqPt
-    
-    p=U{layerNb}.meta.j(k)+1;
-    
-    if length(U{layerNb}.signal{k}) < nbTimePt
-        %Regularize resolution if not the same
-        R= nbTimePt/length(U{layerNb}.signal{k});
-        img(p,:)=(interp(U{layerNb}.signal{k},R))';
-        img(p,:)=log(abs(img(p,:)+epsilon));
-        
-    else img(p,:)=(log(abs((U{layerNb}.signal{k})')+epsilon));
-        
-    end
-    
-    
+function img = scat_img(X,j,epsilon)
+	nbTimePt = max(cellfun(@(x)(size(x,1)), X.signal));
+	
+	max_jm = max(X.meta.j(end,:)+1);
+	
+	img = ones(max_jm, nbTimePt)*log(epsilon);
+	
+	ind = find(all(bsxfun(@eq,X.meta.j(1:end-1,:),j),1));
+	
+	signal = cellfun(@(x)(interp(x,nbTimePt/size(x,1))), ...
+		X.signal(ind),'UniformOutput',false);
+	
+	img(X.meta.j(end,ind)+1,:) = log(abs([signal{:}].')+epsilon);
 end
-end
-
-
-function img=scattergram_2(U,layerNb,j1,epsilon)
-
-%display the scattergram of associated to the layer layerNb for j1 from 
-%the layer layerNb-1 fixed. Note that the
-%order of the scattering coefficient is equal to layerNb-1. Here layerNb >2
-
-
-rU=renorm_scat(U);
-
-nbTimePt=length(U{layerNb}.signal{1}); % this value may vary with time
-maxJ2=max(U{layerNb}.meta.j(2,:)+1);
-img=zeros(maxJ2,nbTimePt);
-img=log(img+epsilon);
-ind=find(U{layerNb}.meta.j(1,:)==j1);
-
-for k=1:length(ind)
-   p=U{layerNb}.meta.j(2,ind(k))+1;
-   img(p,:)=(log(abs(rU{layerNb}.signal{ind(k)})+epsilon))';
-end
-
-
-end
-
