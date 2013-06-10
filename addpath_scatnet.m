898a9d08717343ec45fdccc9db0737a564937e4b
laurentsifre <laurent.sifre@gmail.com>
2013-06-10 13:26:29 +0200

    correct addpath

--- addpath_scatnet.m
+++ addpath_scatnet.m
@@ -7,8 +7,9 @@ addpath(fullfile(path_to_scattlab, 'display'));
 addpath(fullfile(path_to_scattlab, 'filters'));
 addpath(fullfile(path_to_scattlab, 'utils'));
 addpath(fullfile(path_to_scattlab, 'wrappers'));
-addpath(fullfile(path_to_scattlab, 'demo'));
-addpath(fullfile(path_to_scattlab, 'demo/GIS'));
+addpath(fullfile(path_to_scattlab, 'papers'));
+addpath(fullfile(path_to_scattlab, 'papers/GIS'));
+addpath(fullfile(path_to_scattlab, 'papers/DSS'));
 
 clear path_to_scattlab;
 
