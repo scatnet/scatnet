b103e4a3760f24757c3e624ba8aae334f4f5bd35
laurentsifre <laurent.sifre@gmail.com>
2013-06-10 14:02:13 +0200

    test sact utils

--- addpath_scatnet.m
+++ addpath_scatnet.m
@@ -3,6 +3,7 @@ path_to_scattlab = fileparts(mfilename('fullpath'));
 addpath(fullfile(path_to_scattlab, 'classification'));
 addpath(fullfile(path_to_scattlab, 'convolution'));
 addpath(fullfile(path_to_scattlab, 'core'));
+addpath(fullfile(path_to_scattlab, 'scatutils'));
 addpath(fullfile(path_to_scattlab, 'display'));
 addpath(fullfile(path_to_scattlab, 'filters'));
 addpath(fullfile(path_to_scattlab, 'utils'));
