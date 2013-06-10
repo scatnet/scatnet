7113d43e1f28cc78dcbdf33e46af5235e1505e90
laurentsifre <laurent.sifre@gmail.com>
2013-06-10 19:09:45 +0200

    refactor roto-trans

--- test/core/test_scat_3d.m
+++ test/core/test_scat_3d.m
@@ -1,26 +1,15 @@
+% this script demonstrates how to compute the roto-translation
+% scattering transform of an image
 
-N = 256;
-x = zeros(N,N);
+x = uiuc_sample;
 
-step_grid = 64;
-for i = 1:step_grid:N
-	x(:,i) = 1;
-	x(i,:) = 1;
-end
+filt_opt.null = 1;
+filt_rot_opt.null = 1;
+scat_opt.oversampling = 0;
 
+[wavelet, filters, filters_rot ] = ...
+		wavelet_factory_3d(size(x), filt_opt, filt_rot_opt, scat_opt);
 
-%%
-%x = imreadBW('ens.png');
-%x = uiuc_sample;
-%x  = x(1:end/2, 1:end/2);
-options.antialiasing = 1;
-options.precision_4byte = 1;
-%wavelet = wavelet_factory_3d(size(x), options);
- [ wavelet, filters, filters_rot ] = ...
-		wavelet_factory_3d(size(x), options);
-profile on;
 tic;
 [S, U] = scat(x, wavelet);
-toc;
-profile off;
-profile viewer;
+toc
