8074331788037aecafc36f9ecfca563b6290e2c0
Joakim Andén <joakim.anden@gmail.com>
2013-07-04 16:19:17 +0200

    fix feature_wrapper bug in DSS_Table2_TIMIT_m3

--- papers/DSS/DSS_Table2_TIMIT_m3.m
+++ papers/DSS/DSS_Table2_TIMIT_m3.m
@@ -18,21 +18,29 @@ sc1_opt.M = 3;
 Wop = wavelet_factory_1d(N, filt1_opt, sc1_opt);
 
 scatt_fun = @(x)(format_scat(log_scat(renorm_scat(scat(x,Wop)))));
-feature_fun = @(x,obj)(feature_wrapper(x,obj,scatt_fun,N,T_s,2,1));
 
 duration_fun = @(x,obj)(32*duration_feature(x,obj));
 
-features = {feature_fun, duration_fun};
+features = {scatt_fun, duration_fun};
 
 for k = 1:length(features)
 	fprintf('testing feature #%d...',k);
 	tic;
-	sz = size(features{k}(randn(N,1)));
+	if nargin(features{k}) == 1
+		sz = size(features{k}(randn(N,1)));
+	else
+		sz = size(features{k}(randn(N,1),struct('u1',1,'u2',N)));
+	end
 	aa = toc;
 	fprintf('OK (%.2fs) (size [%d,%d])\n',aa,sz(1),sz(2));
 end
 
-db = prepare_database(src,features);
+database_opt.input_sz = N;
+database_opt.output_sz = T_s;
+database_opt.obj_normalize = 2;
+database_opt.collapse = 1;
+
+db = prepare_database(src,features,database_opt);
 db.features = single(db.features);
 db = svm_calc_kernel(db,'gaussian','triangle',[db.indices{train_set}]);
 
