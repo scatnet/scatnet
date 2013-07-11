4b1e9430bb4c420c1892964f3b07d1888ef2b98e
janden <joakim.anden@gmail.com>
2013-07-11 17:57:44 +0200

    make renorm_scat work for differing resolutions

--- scatutils/renorm_scat.m
+++ scatutils/renorm_scat.m
@@ -20,8 +20,14 @@ function X = renorm_scat(X,epsilon,min_order)
 		for p2 = 1:length(X{m+1}.signal)
 			j = X{m+1}.meta.j(:,p2);
 			p1 = find(all(bsxfun(@eq,X{m}.meta.j,j(1:m-1)),1));
-			sub_multiplier = 2^(X{m}.meta.resolution(p1)/2);
-			X{m+1}.signal{p2} = X{m+1}.signal{p2}./(X{m}.signal{p1}+epsilon*sub_multiplier);
+			sub_multiplier = 2^(X{m+1}.meta.resolution(p2)/2);
+			ds = log2(size(X{m}.signal{p1},1)/size(X{m+1}.signal{p2},1));
+			if ds > 0
+				parent = X{m}.signal{p1}(1:2^ds:end,:,:)*2^(ds/2);
+			else
+				parent = interpft(X{m}.signal{p1},size(X{m}.signal{p1},1)*2^(-ds))*2^(-ds/2);
+			end
+			X{m+1}.signal{p2} = X{m+1}.signal{p2}./(parent+epsilon*sub_multiplier);
 		end
 	end
 end
