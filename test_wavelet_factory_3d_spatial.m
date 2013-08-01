74e01c3f21da9dba33868cc7dc0e43db2b1627b5
laurentsifre <laurent.sifre@gmail.com>
2013-08-01 14:48:48 +0200

    test of rotational invariance for scat 3d spatial

--- test_wavelet_factory_3d_spatial.m
+++ test_wavelet_factory_3d_spatial.m
@@ -2,7 +2,7 @@ clear
 x = uiuc_sample;
 %%
 options.J = 3;
-options.precision = 'single';
+options.precision = 'double';
 tic;
 [Wop,f1,f2] = wavelet_factory_3d_spatial(options, options, options);
 toc;
@@ -21,11 +21,21 @@ toc;
 
 
 %%
+sx = format_scat(Sx);
+sx2 = format_scat(Sx2);
+
+ssx = sum(sum(sx,3),2);
+ssx2 = sum(sum(sx2,3),2);
+plot([ssx, ssx2]);
+plot(log([ssx, ssx2]));
+legend('vanilla', 'spatial');
+
+%%
 immac(image_scat_layer(Sx{2}, 0, 0),1);
 immac(image_scat_layer(Sx2{2}, 0, 0),2);
 
 %%
-immac(image_scat_layer(Sx{3}, 1, 1),1);
+immac(image_scat_layer(Sx{3}, 0, 1),1);
 
-immac(image_scat_layer(Sx2{3}, 1, 1),2);
+immac(image_scat_layer(Sx2{3}, 0, 1),2);
 
