d098578957320e935ff84da94f92233f03d6417b
laurentsifre <laurent.sifre@gmail.com>
2013-09-20 10:58:57 +0200

    renorm parent node for roto-translaition scattering

--- spatial/renorm_parent_2d.m
+++ spatial/renorm_parent_2d.m
@@ -1,14 +1,12 @@
 function Sx_rn = renorm_parent_2d(Sx)
-	Sx_rn = Sx;
-	if (isfield(Sx{2}.meta, 'theta')) %2d scat
-		parent = @(p)(find(Sx{2}.meta.j(1,:) == Sx{3}.meta.j(1,p) &...
-			Sx{2}.meta.theta(1,:) == Sx{3}.meta.theta(1,p) & ...
-            Sx{2}.meta.q(1,:) == Sx{3}.meta.q(1,p)));
-	else % 3d scat
-		parent = @(p)(find(Sx{2}.meta.j(1,:) == Sx{3}.meta.j(1,p)));
-	end
-	for p = 1:numel(Sx{3}.signal)
-		Sx_rn{3}.signal{p} = Sx_rn{3}.signal{p}./Sx{2}.signal{parent(p)};
-	end
-	
+Sx_rn = Sx;
+
+parent = @(p)(find(Sx{2}.meta.j(1,:) == Sx{3}.meta.j(1,p) &...
+    Sx{2}.meta.theta(1,:) == Sx{3}.meta.theta(1,p) & ...
+    Sx{2}.meta.q(1,:) == Sx{3}.meta.q(1,p)));
+
+for p = 1:numel(Sx{3}.signal)
+    Sx_rn{3}.signal{p} = Sx_rn{3}.signal{p}./Sx{2}.signal{parent(p)};
+end
+
 end
--- spatial/test_classif_uiuc_renorm.m
+++ spatial/test_classif_uiuc_renorm.m
@@ -12,23 +12,41 @@ fun = @(filename)(scat(imreadBW(filename), Wop));
 all_feat = srcfun(fun, src);
 
 
-%% renorm L1 with smoothing 
-op = renorm_factory_L2_smoothing(5);
-%scat_renorm = @(x)(renorm_sibling_2d(scat(x, w), op));
-renorm = @(x)(renorm_sibling_2d(x, op));
+%%
+%% roto-translation
+src = uiuc_src;
+options.J = 5;
+options.Q = 1;
+options.M = 2;
+
+options.parallel = 0;
+Wop = wavelet_factory_3d_spatial(options, options, options);
+
+%% compute all scattering
+fun = @(filename)(scat(imreadBW(filename), Wop));
+all_feat = srcfun(fun, src);
+
+
+
+%% renorm L1/L2 with smoothing 
+op = renorm_factory_L2_smoothing(3);
+renorm = @(x)(renorm_sibling_3d(x, op));
 feat_renorm = cellfun_monitor(renorm, all_feat);
 
-%% log renorm
+% %% log renorm
 %renorm = @(x)(scatfun(@log, x));
 %feat_renorm = cellfun_monitor(renorm, all_feat);
 
-%% renorm parent
-renorm = @(x)(renorm_parent_2d(x));
-feat_renorm = cellfun_monitor(renorm, all_feat);
+% %% renorm parent
+%%renorm = @(x)(renorm_parent_3d(x));
+%%feat_renorm = cellfun_monitor(renorm, all_feat);
 
-%% spatial average
+% spatial average
 vec = @(Sx)(sum(sum(format_scat(Sx),2),3));
 feat_vec = cellfun_monitor(vec ,feat_renorm);
 
-%% format for classif
+% format for classif
 db = cellsrc2db(feat_vec, src);
+
+%%
+db.features = db.features(2:end,:);
