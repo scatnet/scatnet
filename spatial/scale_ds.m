ca7743058025e9cee354487fabe4fffc1f5de248
laurentsifre <laurent.sifre@gmail.com>
2013-09-23 15:48:18 +0200

    bigfux scale_ds

--- spatial/scale_ds.m
+++ spatial/scale_ds.m
@@ -2,7 +2,7 @@ function [xds,h] = scale_ds(x, scale_fac, nb_scale)
 xds{1} = x;
 for i = 1:nb_scale-1
     step = scale_fac^i;
-    options.sigma_phi = sqrt(step^2 -1)/0.5;
+    options.sigma_phi = sqrt(step^2 -1)*0.5;
     options.P = 2 + floor(2*step);
     filters = morlet_filter_bank_2d_spatial(options);
     
--- spatial/test_multiscale.m
+++ spatial/test_multiscale.m
@@ -1,6 +1,9 @@
 x = draw_circle([256,256], [128,128], 32, 2);
 %%
-x = lena
+x = zeros([512,512]);
+x(10,10) = 1;
+x = real(ifft2(x));
+x = x./max(x(:));
 %% spatial
 src = uiuc_src;
 options.J = 3;
@@ -14,9 +17,15 @@ fun = @(x)(scat(x, Wop));
 multi_fun = @(x)(fun_multiscale(fun, x, sqrt(2), 4));
 [Sx, xms] = multi_fun(x);
 %%
-renorm = @(x)(scatfun(@log, x));
+renorm = @(x)(scatfun(@(x)(log(x+1E-20)), x));
 renorm_ms = @(x)(cellfun_monitor(renorm, x));
 
-vec = @(Sx)(mean(mean(remove_margin(format_scat(Sx),1),2),3));
+Sx_renorm = renorm_ms(Sx);
+%%
+
+vec = @(x)(mean(mean(remove_margin(format_scat(x),1),2),3));
 vec_ms = @(x)(cellfun_monitor(vec, x));
-Sx_spat_avg = vec_ms(Sx)
+Sx_spat_avg = vec_ms(Sx_renorm);
+sx_spat_avg = cell2mat(Sx_spat_avg);
+plot(sx_spat_avg);
+legend('1','2','3','4');
