b79783534a6e48b8dfd1470068360131535b5203
laurentsifre <laurent.sifre@gmail.com>
2013-09-24 17:12:03 +0200

    precomputed features

--- papers/RSDS/table_1.m
+++ papers/RSDS/table_1.m
@@ -1,6 +1,6 @@
-% a script to reproduce table 1 of paper 
+% a script to reproduce table 1 of paper :
 %
-%   ``Rotation, Scaling and Deformation Invariant Scattering 
+%   ``Rotation, Scaling and Deformation Invariant Scattering
 %   for Texture Discrimination"
 %   Laurent Sifre, Stephane Mallat
 %   Proc. IEEE CVPR 2013 Portland, Oregon
@@ -10,13 +10,15 @@
 % NOTE THAT MAY SAVE YOU A LOT OF TIME : computing the scattering for the
 % whole database takes time. We provide precomputed scattering in the files
 %   precomputed/kth-tips/trans_scatt.mat
-%   precomputed/kth-tips/roto-trans_scatt.mat
-%   precomputed/kth-tips/roto-trans_scatt_log.mat
-%   precomputed/kth-tips/roto-trans_scatt_log_scale_avg.mat
-%   precomputed/kth-tips/roto-trans_scatt_log_scale_avg_multiscal_train.mat
+%   precomputed/kth-tips/roto_trans_scatt_all.mat
+%   precomputed/kth-tips/roto_trans_scatt_log.mat
+%   precomputed/kth-tips/roto_trans_scatt_log_scale_avg.mat
+%   precomputed/kth-tips/roto_trans_scatt_log_scale_avg_multiscal_train.mat
 % If you want to save time, you can load those files into MATLAB workspace
 % and proceed directly to the classification step.
-
+%
+% DOWNLOAD : The KTH-TIPS databased can be downloaded at
+%   http://www.nada.kth.se/cvap/databases/kth-tips/kth_tips_grey_200x200.tar
 
 
 %% ---------------------------------------------------
@@ -27,45 +29,51 @@
 
 %% load the database
 clear; close all;
-% WARNING : the following line must be modified with the path to the 
+% WARNING : the following line must be modified with the path to the
 % KTH-TIPS database in YOUR system.
 path_to_db = '/Users/laurentsifre/TooBigForDropbox/Databases/KTH_TIPS';
 src = kthtips_src(path_to_db);
 
 
-%% configure scattering
-options.J = 4; % number of octaves
-options.Q = 1; % number of scales per octave
-options.M = 2; % scattering orders
-
-% build the wavelet transform operators for scattering
-Wop = wavelet_factory_2d_spatial(options, options);
-
-% a function handle that 
-%   - read the image
-%   - compute its scattering
-fun = @(filename)(scat(imreadBW(filename), Wop));
-
 %% compute scattering of all images in the database
-% CAN BE SKIPED with load('kth-tips_trans_scatt.mat')
-
-% scattering
-% (328 seconds on a 2.4 Ghz Intel Core i7)
-trans_scatt_all = srcfun(fun, src);
-
-% margin removal and global spatial average
-% Note : due to boundary effects, margin removal has a non-negligible
-% positive impact on classification results. Skiping this step
-% will results in performance drop.
-% (10 seconds on a 2.4 Ghz Intel Core i7)
-vec = @(Sx)(mean(mean(remove_margin(format_scat(Sx),1),2),3));
-trans_scatt = cellfun_monitor(vec ,trans_scatt_all);
-
-%% save scattering
-save('./precomputed/kth-tips/trans_scatt.mat', 'trans_scatt');
-
-%% format the database of feature
-db = cellsrc2db(trans_scatt, src);
+use_precompted_scattering = 1; % change to 0 to skip computation of scattering
+if (use_precompted_scattering)
+    load('./precomputed/kth-tips/trans_scatt.mat');
+else
+    %configure scattering
+    options.J = 5; % number of octaves
+    options.Q = 1; % number of scales per octave
+    options.M = 2; % scattering orders
+    
+    % build the wavelet transform operators for scattering
+    Wop = wavelet_factory_2d_spatial(options, options);
+    
+    % a function handle that
+    %   - read the image
+    %   - resize it to 200x200
+    %   - compute its scattering
+    fun = @(filename)(scat(imresize_notoolbox(imreadBW(filename)...
+        ,[200 200]), Wop));
+    
+    % compute all scattering
+    % (500 seconds on a 2.4 Ghz Intel Core i7)
+    trans_scatt_all = srcfun(fun, src);
+    
+    % remove margin and globaly average accross position
+    % Note : due to boundary effects, margin removal has a non-negligible
+    % positive impact on classification results. Skiping this step
+    % will results in performance drop.
+    % (10 seconds on a 2.4 Ghz Intel Core i7)
+    fun = @(Sx)(mean(mean(remove_margin(format_scat(Sx),1),2),3));
+    trans_scatt = cellfun_monitor(fun ,trans_scatt_all);
+    
+    % save scattering
+    %save('./precomputed/kth-tips/trans_scatt.mat', 'trans_scatt');
+    
+    % format the database of feature
+    db = cellsrc2db(trans_scatt, src);
+    
+end
 
 %% classification
 grid_train = [5,20,40];
@@ -74,17 +82,17 @@ n_fold = 10; % Note : very similar results can be obtained with 200 folds.
 clear error_2d;
 
 for i_fold = 1:n_fold
-	for i_grid = 1:numel(grid_train)
-		n_train = grid_train(i_grid);
-		prop = n_train/n_per_class ;
-		[train_set, test_set] = create_partition(src, prop);
-		train_opt.dim = n_train;
-		model = affine_train(db, train_set, train_opt);
-		labels = affine_test(db, model, test_set);
-		error_2d(i_fold, i_grid) = classif_err(labels, test_set, src);
-		fprintf('fold %d n_train %g acc %g \n', ...
+    for i_grid = 1:numel(grid_train)
+        n_train = grid_train(i_grid);
+        prop = n_train/n_per_class ;
+        [train_set, test_set] = create_partition(src, prop);
+        train_opt.dim = n_train;
+        model = affine_train(db, train_set, train_opt);
+        labels = affine_test(db, model, test_set);
+        error_2d(i_fold, i_grid) = classif_err(labels, test_set, src);
+        fprintf('fold %d n_train %g acc %g \n', ...
             i_fold, n_train, 1-error_2d(i_fold, i_grid));
-	end
+    end
 end
 
 %% averaged performance
@@ -96,10 +104,9 @@ for i_grid = 1:numel(grid_train)
         grid_train(i_grid), perf(i_grid), perf_std(i_grid));
 end
 % expected output :
-%    kth-tips trans scatt with  5 training : 67.26 += 5.68 
-%    kth-tips trans scatt with 20 training : 94.89 += 1.59 
-%    kth-tips trans scatt with 40 training : 98.54 += 0.71 
-
+%   kth-tips trans scatt with  5 training : 69.84 += 3.40
+%   kth-tips trans scatt with 20 training : 95.03 += 1.08
+%   kth-tips trans scatt with 40 training : 98.10 += 0.79
 
 
 
@@ -111,8 +118,85 @@ end
 
 %% load the database
 clear; close all;
-% WARNING : the following line must be modified with the path to the 
+% WARNING : the following line must be modified with the path to the
 % KTH-TIPS database in YOUR system.
 path_to_db = '/Users/laurentsifre/TooBigForDropbox/Databases/KTH_TIPS';
 src = kthtips_src(path_to_db);
+
+
+%% compute scattering of all images in the database
+use_precompted_scattering = 1; % change to 0 to skip computation of scattering
+if (use_precompted_scattering)
+    load('./precomputed/kth-tips/roto_trans_scatt.mat');
+else
+    % configure scattering
+    options.J = 5; % number of octaves
+    options.Q = 1; % number of scales per octave
+    options.M = 2; % scattering orders
+    
+    % build the wavelet transform operators for scattering
+    Wop = wavelet_factory_3d_spatial(options, options, options);
+    
+    % a function handle that
+    %   - read the image
+    %   - resize it to 200x200
+    %   - compute its scattering
+    fun = @(filename)(scat(imresize_notoolbox(imreadBW(filename),[200 200]), Wop));
+    
+    % compute scattering of all images in the database
+    % CAN BE SKIPED by loading precomputed/kth-tips/trans_scatt.mat
+    
+    % scattering
+    % (800 seconds on a 2.4 Ghz Intel Core i7)
+    roto_trans_scatt_all = srcfun(fun, src);
+    
+    % margin removal and global spatial average
+    % Note : due to boundary effects, margin removal has a non-negligible
+    % positive impact on classification results. Skiping this step
+    % will results in performance drop.
+    % (10 seconds on a 2.4 Ghz Intel Core i7)
+    fun = @(Sx)(mean(mean(remove_margin(format_scat(Sx),1),2),3));
+    roto_trans_scatt = cellfun_monitor(fun ,roto_trans_scatt_all);
+    
+    % save scattering
+    %save('./precomputed/kth-tips/roto_trans_scatt.mat', 'roto_trans_scatt');
+    
+    % format the database of feature
+    db = cellsrc2db(roto_trans_scatt, src);
+end
+
+%% classification
+grid_train = [5,20,40];
+n_per_class = 81;
+n_fold = 10; % Note : very similar results can be obtained with 200 folds.
+clear error_2d;
+
+for i_fold = 1:n_fold
+    for i_grid = 1:numel(grid_train)
+        n_train = grid_train(i_grid);
+        prop = n_train/n_per_class ;
+        [train_set, test_set] = create_partition(src, prop);
+        train_opt.dim = n_train;
+        model = affine_train(db, train_set, train_opt);
+        labels = affine_test(db, model, test_set);
+        error_2d(i_fold, i_grid) = classif_err(labels, test_set, src);
+        fprintf('fold %d n_train %g acc %g \n', ...
+            i_fold, n_train, 1-error_2d(i_fold, i_grid));
+    end
+end
+
+%% averaged performance
+perf = 100*(1-mean(error_2d));
+perf_std = 100*std(error_2d);
+
+for i_grid = 1:numel(grid_train)
+    fprintf('kth-tips roto-trans scatt with %2d training : %.2f += %.2f \n', ...
+        grid_train(i_grid), perf(i_grid), perf_std(i_grid));
+end
+% expected output :
+%   kth-tips roto-trans scatt with  5 training : 71.01 += 3.51
+%   kth-tips roto-trans scatt with 20 training : 94.80 += 1.15
+%   kth-tips roto-trans scatt with 40 training : 99.29 += 0.54
+
+
 
