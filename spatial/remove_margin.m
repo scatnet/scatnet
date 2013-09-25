207268d1d4562786df1c19687f132cc060dd294a
laurentsifre <laurent.sifre@gmail.com>
2013-09-25 11:46:16 +0200

    doc remove margin

--- spatial/remove_margin.m
+++ spatial/remove_margin.m
@@ -1,4 +1,18 @@
-% remove_margin
+% remove_margin : remove margins along specified dimensions
+% 
+% Input 
+%   - x (numeric) : the array (up to 3d) for which to remove the margin
+%   - n (positive integer) : the size of margin to remove
+%   - dims (array) : the dimensions on which to remove the margins
+%
+% Output
+%   - x2 (numeric) : the array with removed margin
+%
+% Usage
+%   x2 = remove_margin(x, 1, [2,3]) 
+%       will remove 1 elements at begining and end of array along dimension
+%       and 3.
+
 function x2 = remove_margin(x, n, dims)
     [N,M,P] = size(x);
     x2 = x;
