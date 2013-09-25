e6d783756990e8c92b6bc030740ba1dbd3da7501
laurentsifre <laurent.sifre@gmail.com>
2013-09-25 14:06:05 +0200

    remove useless files

--- spatial/test_merge_fieldnames.m
+++
@@ -1,6 +0,0 @@
-clear;
-meta1.j = [1,2,3,4];
-
-meta2.j = [1,2,3,4,5,6;1,2,3,4,5,6];
-meta2.theta = [1,2,3,4,5,6];
-[fn, nfn1, nfn2] = merge_fieldnames(meta1, meta2);
--- spatial/test_pca_reduce.m
+++
@@ -1,4 +0,0 @@
-
-db2.features = log(db.features(:,:));
-cut = 200;
-[db2,u] = pca_cut(db2,cut); 
