813129d7c80cc625ca08f13b1e530a35a9608986
laurentsifre <laurent.sifre@gmail.com>
2013-09-30 16:33:39 +0200

    debuged pad miror to handle corner cases

--- convolution/pad_mirror_2d_twosided.m
+++ convolution/pad_mirror_2d_twosided.m
@@ -1,7 +1,33 @@
 function x_pad = pad_mirror_2d_twosided(x, marg)
+    %%
     if (~isnumeric(marg) || numel(marg)<2)
-       error('margin for padding should be a 2x1 matrix'); 
+        error('margin for padding should be a 2x1 matrix');
     end
-	x = [x(marg(1)+1:-1:2,:); x; x(end-1:-1:end-marg(1),:)];
-	x_pad = [x(:,marg(2)+1:-1:2), x, x(:,end-1:-1:end-marg(2))];
+    [n,m] = size(x);
+    if (n == 1)
+        x = repmat(x, [marg(1) 1]);
+        n = marg(1);
+    else
+        while (n <= marg(1)) % full mirror copy
+            x = [x(n:-1:2,:); x; x(n-1:-1:1,:)];
+            marg(1) = marg(1)-(n-1);
+            n = n + 2*(n-1);
+        end
+        x = [x(marg(1)+1:-1:2,:); x; x(end-1:-1:end-marg(1),:)];
+    end
+    
+    
+    if (m == 1)
+        x = repmat(x, [2 marg(2)]);
+        m = marg(2);
+    else
+        while (m <= marg(2)) % full mirror copy
+            x = [x(:,m:-1:2), x, x(:,m-1:-1:1)];
+            marg(2) = marg(2)-(m-1);
+            m = m + 2*(m-1);
+        end
+        x_pad = [x(:,marg(2)+1:-1:2), x, x(:,end-1:-1:end-marg(2))];
+    end
+    
+    
 end
--- unittest/convolution/test_pad_mirror_2d_twosided.m
+++ unittest/convolution/test_pad_mirror_2d_twosided.m
@@ -8,23 +8,36 @@ classdef test_pad_mirror_2d_twosided < matlab.unittest.TestCase
         function testBasic(testcase)
             %%
             x = rand(17,17);
-            P = 3;
-            pad_mirror_2d_twosided(x,P)
-            
-            %% assert same size
-            
-            
+            margins = [3, 3];
+            x_paded = pad_mirror_2d_twosided(x, margins);
+            %% Assert correct size
+            actual = size(x_paded);
+            expected = size(x) + 2 * margins;
+            %%
+            testcase.assertEqual(actual, expected);
         end
         
         
         function testIllegalArg(testcase)
-            %%
+            %% 
             x = rand(17,17);
-            P = 3;
-            pad_mirror_2d_twosided(x,P)
-            
-            %% assert same size
-            
+            margins = 3;
+            didError = 0;
+            try 
+               pad_mirror_2d_twosided(x, margins);
+            catch
+                didError = 1;
+            end
+            %%
+            testcase.assertEqual(didError, 1);
+        end
+        
+        function testSizeSmallerThanMargin(testcase)
+            %%
+            x  = rand(4,4);
+            margins = [10, 10];
+            x_paded = pad_mirror_2d_twosided(x, margins);
+            %%
             
         end
     end
