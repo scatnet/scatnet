8e27dffb34a24404fdbdd5956c57e31c7e47ea70
edouardoyallon <edouardoyallon@dhcp218.dmi.ens.fr>
2013-09-30 15:55:10 +0200

    removed meta
    no more meta_phi, meta_psi + management of the argument of
    wavelet_factory_2d

--- core/wavelet_2d.m
+++ core/wavelet_2d.m
@@ -1,7 +1,7 @@
 % WAVELET_2D Compute the wavelet transform of a signal x
 %
 % Usage
-%    [x_phi, x_psi, meta_phi, meta_psi] = WAVELET_2D(x, filters, options)
+%    [x_phi, x_psi] = WAVELET_2D(x, filters, options)
 %
 % Input
 %    x (numeric): the input signal
@@ -11,8 +11,6 @@
 % Output
 %    x_phi (cell): Low pass part of the wavelet transform
 %    x_psi (cell): Wavelet coeffcients of the wavelet transform
-%    meta_phi (structure): meta information about x_phi
-%    meta_psi (structure): meta information about x_psi
 %
 % Description
 %    WAVELET_2D computes a wavelet transform, using the signal and the
@@ -26,7 +24,7 @@
 %   WAVELET_2D, CONV_SUB_2D, WAVELET_FACTORY_2D_PYRAMID
 
 
-function [x_phi, x_psi, meta_phi, meta_psi] = wavelet_2d(x, filters, options)
+function [x_phi, x_psi] = wavelet_2d(x, filters, options)
 	if nargin<3
 		options = struct();
 	end
@@ -54,15 +52,15 @@ function [x_phi, x_psi, meta_phi, meta_psi] = wavelet_2d(x, filters, options)
 	x_phi = real(conv_sub_2d(xf, filters.phi.filter, ds));
 	x_phi = unpad_signal(x_phi, ds*[1 1], size(x));
 	
-	meta_phi.j = -1;
-	meta_phi.theta = -1;
-	meta_phi.resolution = lastres+ds;
+	x_phi.meta.j = -1;
+	x_phi.meta.theta = -1;
+	x_phi.meta.resolution = lastres+ds;
 	
 	% high pass filtering, downsampling and unpading
 	x_psi = {};
-	meta_psi.j = -1*ones(1, numel(filters.psi.filter));
-	meta_psi.theta = -1*ones(1, numel(filters.psi.filter));
-	meta_psi.resolution = -1*ones(1, numel(filters.psi.filter));
+	x_psi.meta.j = -1*ones(1, numel(filters.psi.filter));
+	x_psi.meta.theta = -1*ones(1, numel(filters.psi.filter));
+	x_psi.meta.resolution = -1*ones(1, numel(filters.psi.filter));
 	for p = find(psi_mask)
 		j = filters.psi.meta.j(p);
 		ds = max(floor(j/Q)- lastres - oversampling, 0);
@@ -70,9 +68,9 @@ function [x_phi, x_psi, meta_phi, meta_psi] = wavelet_2d(x, filters, options)
 		x_psi{p} = conv_sub_2d(xf, filters.psi.filter{p}, ds);
 		x_psi{p} = unpad_signal(x_psi{p}, ds*[1 1], size(x));
 		
-		meta_psi.j(1,p) = filters.psi.meta.j(p);
-		meta_psi.theta(1,p) = filters.psi.meta.theta(p);
-		meta_psi.resolution(1,p) = lastres+ds;
+		x_psi.meta.j(1,p) = filters.psi.meta.j(p);
+		x_psi.meta.theta(1,p) = filters.psi.meta.theta(p);
+		x_psi.meta.resolution(1,p) = lastres+ds;
 	end
 	
 	if(precision_4byte)
