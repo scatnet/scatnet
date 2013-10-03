9bf2638120dd97b2953f8a3156dd1f08cf36a482
Joakim Andén <joakim.anden@gmail.com>
2013-10-03 18:09:03 +0200

    fix typo in filter_freq

--- filters/filter_freq.m
+++ filters/filter_freq.m
@@ -5,6 +5,6 @@ function [psi_xi,psi_bw,phi_bw] = filter_freq(filters)
 		strcmp(filters.meta.filter_type,'gabor_1d')
 		[psi_xi,psi_bw,phi_bw] = morlet_freq_1d(filters);
 	else
-		error(sprintf('Unknown filter type ''%s''',filters.metafilter_type))
+		error(sprintf('Unknown filter type ''%s''',filters.meta.filter_type))
 	end
 end
