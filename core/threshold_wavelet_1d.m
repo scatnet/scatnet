ca6214a561f6e05b3a99169917c3e8f8fcb6c101
kapokmi <michelkapoko@gmail.com>
2013-10-03 08:54:25 +0200

    added links to related functions in the help

--- core/threshold_wavelet_1d.m
+++ core/threshold_wavelet_1d.m
@@ -1,4 +1,4 @@
-% wavelet_1d: Thresholded 1D wavelet transform.
+% THRESHOLD_1D_WAVELET: Thresholded 1D wavelet transform.
 % Usage
 %    [x_phi, x_psi, meta_phi, meta_psi] = threshold_wavelet_1d(x, ...
 %       filters, options)
@@ -12,29 +12,29 @@
 %    x_phi: x filtered by lowpass filter phi
 %    x_psi: cell array of x filtered by wavelets psi
 %    meta_phi, meta_psi: meta information on x_phi and x_psi, respectively
+% See also  WAVELET_1D
+
 
 function [x_phi, x_psi, meta_phi, meta_psi] = threshold_wavelet_1d(x, ...
-	filters, options)
-	
-	if nargin < 3
-		options = struct();
-	end
+    filters, options)
 
-	options = fill_struct(options, 'threshold', 1e-4);
-	
-	[x_phi, x_psi, meta_phi, meta_psi] = wavelet_1d(x, filters, options);
-	
-	for p1 = find(~cellfun(@isempty, x_psi))
-		res = meta_psi.resolution(p1);
-		
-		x_psi{p1 ...
-		} = T_theta(x_psi{p1}, options.threshold*2^(res/2));
-	end
+if nargin < 3
+    options = struct();
+end
+
+options = fill_struct(options, 'threshold', 1e-4);
+
+[x_phi, x_psi, meta_phi, meta_psi] = wavelet_1d(x, filters, options);
+
+for p1 = find(~cellfun(@isempty, x_psi))
+    res = meta_psi.resolution(p1);
+    
+    x_psi{p1 ...
+        } = T_theta(x_psi{p1}, options.threshold*2^(res/2));
+end
 end
 
 function y = T_theta(x, theta)
-	r = abs(x);
-	r(r>theta) = r(r>theta)-theta;
-	r(r<=theta) = 0;
-	y = x./abs(x).*r;
+y=sign(x).*max(0,abs(x)-theta);
+
 end
