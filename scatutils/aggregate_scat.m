007c1f9f11e94bb2c3a8e90d996d683517bf1a71
kapokmi <michelkapoko@gmail.com>
2013-10-03 08:57:12 +0200

    added link to related functions in the help

--- scatutils/aggregate_scat.m
+++ scatutils/aggregate_scat.m
@@ -1,13 +1,14 @@
-
-% aggregate_scat: Aggregate successive frames of a scattering transform.
+%
+% AGGREGATE_SCAT: Aggregate successive frames of a scattering transform.
 % Usage
-%    S = aggregage_scat(S,N)
+%    S = aggregate_scat(S,N)
 % Input
 %    S: A scattering transform.
 %    N: The length of the window with which to aggregate.
 % Output
 %    S: The scattering transform with successive frames within a window of
 %       length N aggregated into one frame, multiplying the number of paths.
+% See also AVERAGE_SCAT,FLATTEN_SCAT
 
 function Y = aggregate_scat(X,N)
 for m = 0:length(X)-1
@@ -25,31 +26,29 @@ for m = 0:length(X)-1
         % determine the window size at the current resolution
         N1 = N/2^(X{m+1}.meta.resolution(p1));
         % copy metas of this signal into metas of multiple signals
-        
         Y{m+1}.meta = map_meta(X{m+1}.meta,p1,Y{m+1}.meta,r1:r1+N1-1);
+        
         % extract set of frames of length N1
-        %s=size(signal,3);
         
         if size(signal,3)>1
             signal2=zeros(N1,size(signal,1)/N1,size(signal,2),size(signal,3));
             for d3=1:size(signal,3)
-                signal2(:,:,:,d3) = reshape(signal(:,:,d3),[N1 size(signal,1)/N1 size(signal,2)]);  
+                signal2(:,:,:,d3) = reshape(signal(:,:,d3),[N1 size(signal,1)/N1 size(signal,2)]);
             end
             
             signal2 = permute(signal2,[2 3 1 4]);
-          
+            
             for k1=1:N1
                 sig=zeros(size(signal2,1),size(signal2,2),size(signal2,4));
                 for l=1:size(signal2,4)
                     sig(:,:,l)=signal2(:,:,k1,l);
                 end
-                %sig=permute(sig,[1 3 2]);
+                
                 Y{m+1}.signal{r1} = sig;
                 r1 = r1+1;
             end
             
         else signal = reshape(signal,[N1 size(signal,1)/N1 size(signal,2)]);
-            
             signal = permute(signal,[2 3 1]);
             
             for k1 = 1:N1
--- scatutils/average_scat.m
+++ scatutils/average_scat.m
@@ -1,68 +1,68 @@
-% average_scat: Average successive frames of a scattering transform.
+% AVERAGE_SCAT: Average successive frames of a scattering transform.
 % Usage
 %    S = average_scat(S,T,step,window_fun)
 % Input
 %    S: A scattering transform.
 %    T: The length of the window with which to average.
 %    step: The stepping of the successive windows. Default T/2.
-%    window_fun: The windowing function to use for averaging. Default 
-%       @hanning
+%    window_fun: The windowing function to use for averaging. Default
+%       @hanning_standalone
 % Output
 %    S: The scattering transform with each signal averaged over a window of
 %       length N using the window function specified.
+% See also AGGREGATE_SCAT, FLATTEN_SCAT
+
 
 function X = average_scat(X,T,step,window_fun)
-	if nargin < 3
-		step = T/2;
-	end
-	
-	if nargin < 4
-		window_fun = @hanning_standalone;
-	end
-	
-	for m = length(X)-1:-1:0
-		for p1 = 1:length(X{m+1}.signal)
-			if ~isfield(X{m+1}.meta,'resolution')
-				res = 0;
-			else
-				res = X{m+1}.meta.resolution(p1);
-			end
-			N0 = size(X{m+1}.signal{p1},1);
-            
-            
-			if T < 2^res 
-				continue;
-			end
-			% calculate the T and step at current resolution
-			real_T = T/2^res;
-			real_step = max(1,step/2^res);
-			% calculate number of windows we get & allocate buffer
-			n_windows = floor(size(X{m+1}.signal{p1},1)/real_step);
-			buf = zeros([real_T,n_windows,size(X{m+1}.signal{p1},3)]);
-			% since we want symmetric boundary conditions, we need to extend 
-			% the signal symmetrically
-			ind0 = [1:N0 N0:-1:1];
-			% cut out the relevant segments & place in buffer
-			for k = 1:n_windows
-				center = (k-1)*real_step+1;
-				% for a given center & T, calculate the corresponding indices
-				ind = ind0(mod(floor([center-real_T/2+1:center+real_T/2])-1,2*N0)+1);
+if nargin < 3
+    step = T/2;
+end
 
+if nargin < 4
+    window_fun = @hanning_standalone;
+end
+
+for m = length(X)-1:-1:0
+    for p1 = 1:length(X{m+1}.signal)
+        if ~isfield(X{m+1}.meta,'resolution')
+            res = 0;
+        else
+            res = X{m+1}.meta.resolution(p1);
+        end
+        N0 = size(X{m+1}.signal{p1},1);
+        
+        
+        if T < 2^res
+            continue;
+        end
+        % calculate the T and step at current resolution
+        real_T = T/2^res;
+        real_step = max(1,step/2^res);
+        % calculate number of windows we get & allocate buffer
+        n_windows = floor(size(X{m+1}.signal{p1},1)/real_step);
+        buf = zeros([real_T,n_windows,size(X{m+1}.signal{p1},3)]);
+        % since we want symmetric boundary conditions, we need to extend
+        % the signal symmetrically
+        ind0 = [1:N0 N0:-1:1];
+        % cut out the relevant segments & place in buffer
+        for k = 1:n_windows
+            center = (k-1)*real_step+1;
+            % for a given center & T, calculate the corresponding indices
+            ind = ind0(mod(floor([center-real_T/2+1:center+real_T/2])-1,2*N0)+1);
+            
             buf(:,k,:) = X{m+1}.signal{p1}(ind,:,:);
-   
-			end
-			% for each segment, multiply by windowing kernel & sum
-			kernel = window_fun(size(buf,1));
-			buf = bsxfun(@times,buf,kernel/sum(kernel));
-			buf = sum(buf,1);
-			
-			% get rid of dimension 1 & store result in X
-			
+            
+        end
+        % for each segment, multiply by windowing kernel & sum
+        kernel = window_fun(size(buf,1));
+        buf = bsxfun(@times,buf,kernel/sum(kernel));
+        buf = sum(buf,1);
+        
+        % get rid of dimension 1 & store result in X
+        buf =permute(buf,[2 1 3]);
 
-            buf =permute(buf,[2 1 3]);
-           % size(buf)
-			X{m+1}.signal{p1} = buf;
-			X{m+1}.meta.resolution(p1) = res+log2(real_step);
-		end
-	end
+        X{m+1}.signal{p1} = buf;
+        X{m+1}.meta.resolution(p1) = res+log2(real_step);
+    end
+end
 end
