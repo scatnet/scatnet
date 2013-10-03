288f372440c6dd5741b2a2ab529d3a004a59f244
kapokmi <michelkapoko@gmail.com>
2013-10-03 08:53:37 +0200

    added links to related functions in the help

--- classification/svm_train.m
+++ classification/svm_train.m
@@ -1,4 +1,4 @@
-% svm_train: Train an SVM classifier.
+% SVM_TRAIN: Train an SVM classifier.
 % Usage
 %    model = svm_train(db, train_set, options)
 % Input
@@ -11,120 +11,131 @@
 %          options.gamma: The gamma of the Gaussian kernel (default 1e-4).
 %          options.no_inplace: Do not use the inplace version of LIBSVM, even
 %             if it available (default 0).
-%          options.full_test_kernel: Explicity calculate the test kernel 
-%             instead of relying on the precalculated kernel. Used if the 
+%          options.full_test_kernel: Explicity calculate the test kernel
+%             instead of relying on the precalculated kernel. Used if the
 %             kernel is only defined on the training vectors (default 0).
+%          options.w: Add weights to rebalance the training set if it is
+%             imbalanced. The rebalancing is done so that the distribution
+%          of the training samples seem to be uniform for all the classes
+%          (default 0).
+%
+%
 % Output
 %    model: The SVM model.
 % Description
-%    The svm_train function provides an interface to the LIBSVM set of 
-%    SVM training routines. If available, will use the inplace version found 
+%    The svm_train function provides an interface to the LIBSVM set of
+%    SVM training routines. If available, will use the inplace version found
 %    in libsvm-compact (see http://www.di.ens.fr/data/software/) to save
 %    memory and speed up calculations.
+% See also SVM_TEST, CLASSiF_ERR, CLASSIF_RECOG
 
 function model = svm_train(db,train_set,opt)
-	if nargin < 3
-		opt = struct();
-	end
-	
-	%if ~exist('svmtrain_inplace')
-	%	error('you should make sure svmtrain_inplace is available...');
-	%end
+if nargin < 3
+    opt = struct();
+end
 
-	opt = fill_struct(opt,'no_inplace',0);
-	opt = fill_struct(opt,'full_test_kernel',0);
+%if ~exist('svmtrain_inplace')
+%	error('you should make sure svmtrain_inplace is available...');
+%end
 
-	opt = fill_struct(opt,'kernel_type','gaussian');
-	
-	opt = fill_struct(opt,'gamma',1e-4);
-	opt = fill_struct(opt,'C',8);
-    opt = fill_struct(opt, 'w',0);
-    
-	
-	ind_features = [];
-	feature_class = [];
-	for k = 1:length(train_set)
-		ind = db.indices{train_set(k)};
-		ind_features = [ind_features ind];
-		feature_class = [feature_class ...
-			db.src.objects(train_set(k)).class*ones(1,length(ind))];
-	end
+opt = fill_struct(opt,'no_inplace',0);
+opt = fill_struct(opt,'full_test_kernel',0);
 
-	
-	precalc_kernel = isfield(db,'kernel') && ...
-		strcmp(opt.kernel_type,db.kernel.kernel_type);
-	
-	params = ['-q -c ' num2str(opt.C)];
-	if ~precalc_kernel
-		if strcmp(opt.kernel_type,'linear')
-			params = [params ' -t 0'];
-		elseif strcmp(opt.kernel_type,'gaussian')
-			params = [params ' -t 2 -g ' num2str(opt.gamma)];
-		else
-			error('Unknown kernel type!');
-		end
-		features = db.features(:,ind_features);
-	else
-		[kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
+opt = fill_struct(opt,'kernel_type','gaussian');
 
-		ind_features = kernel_ind(ind_features(kernel_mask(ind_features)));
+opt = fill_struct(opt,'gamma',1e-4);
+opt = fill_struct(opt,'C',8);
+opt = fill_struct(opt, 'w',0);
+opt = fill_struct(opt, 'b',0);
 
-		if exist('svmtrain_inplace') && ~opt.no_inplace
-			feature_class = zeros(1,size(db.features,2));
-			for k = 1:length(db.indices)
-				feature_class(db.indices{k}) = db.src.objects(k).class;
-			end
-			feature_class = feature_class(db.kernel.kernel_set);
-			features = db.kernel.K;
+ind_features = [];
+feature_class = [];
+for k = 1:length(train_set)
+    ind = db.indices{train_set(k)};
+    ind_features = [ind_features ind];
+    feature_class = [feature_class ...
+        db.src.objects(train_set(k)).class*ones(1,length(ind))];
+end
 
-			if strcmp(db.kernel.kernel_type,'linear') && ...
-				strcmp(db.kernel.kernel_format,'square')
-				params = [params ' -t 4'];
-			elseif strcmp(db.kernel.kernel_type,'linear') && ...
-				strcmp(db.kernel.kernel_format,'triangle')
-				params = [params ' -t 5'];
-			elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
-				strcmp(db.kernel.kernel_format,'square')
-				params = [params ' -t 6 -g ' num2str(opt.gamma)];
-			elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
-				strcmp(db.kernel.kernel_format,'triangle')
-				params = [params ' -t 7 -g ' num2str(opt.gamma)];
-			else
-				error('Unknown kernel type/format!');
-			end
-		else
-			params = [params ' -t 4'];
-			
-			if strcmp(db.kernel.kernel_format, 'triangle')
-				error(['Triangular kernels not supported for standard ' ...
-				' LIBSVM version. Please try libsvm-compact.'])
-			end
-			features = db.kernel.K(:,ind_features);
-			
-			if strcmp(db.kernel.kernel_type,'gaussian')
-				features(2:end,:) = exp(-opt.gamma*features(2:end,:));
-				params = [params ' -g ' num2str(opt.gamma)];
-			end
-		end
+
+precalc_kernel = isfield(db,'kernel') && ...
+    strcmp(opt.kernel_type,db.kernel.kernel_type);
+
+params = ['-q -c ' num2str(opt.C)];
+if ~precalc_kernel
+    if strcmp(opt.kernel_type,'linear')
+        params = [params ' -t 0'];
+    elseif strcmp(opt.kernel_type,'gaussian')
+        params = [params ' -t 2 -g ' num2str(opt.gamma)];
+    else
+        error('Unknown kernel type!');
     end
+    features = db.features(:,ind_features);
+else
+    [kernel_mask,kernel_ind] = ismember(1:size(db.features,2),db.kernel.kernel_set);
     
+    ind_features = kernel_ind(ind_features(kernel_mask(ind_features)));
     
-    if opt.w == 1
-        db_weights=calc_train_weights(db, train_set);
-        params = [params db_weights];
+    if exist('svmtrain_inplace') && ~opt.no_inplace
+        feature_class = zeros(1,size(db.features,2));
+        for k = 1:length(db.indices)
+            feature_class(db.indices{k}) = db.src.objects(k).class;
+        end
+        feature_class = feature_class(db.kernel.kernel_set);
+        features = db.kernel.K;
+        
+        if strcmp(db.kernel.kernel_type,'linear') && ...
+                strcmp(db.kernel.kernel_format,'square')
+            params = [params ' -t 4'];
+        elseif strcmp(db.kernel.kernel_type,'linear') && ...
+                strcmp(db.kernel.kernel_format,'triangle')
+            params = [params ' -t 5'];
+        elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
+                strcmp(db.kernel.kernel_format,'square')
+            params = [params ' -t 6 -g ' num2str(opt.gamma)];
+        elseif strcmp(db.kernel.kernel_type,'gaussian') && ...
+                strcmp(db.kernel.kernel_format,'triangle')
+            params = [params ' -t 7 -g ' num2str(opt.gamma)];
+        else
+            error('Unknown kernel type/format!');
+        end
+    else
+        params = [params ' -t 4'];
+        
+        if strcmp(db.kernel.kernel_format, 'triangle')
+            error(['Triangular kernels not supported for standard ' ...
+                ' LIBSVM version. Please try libsvm-compact.'])
+        end
+        features = db.kernel.K(:,ind_features);
+        
+        if strcmp(db.kernel.kernel_type,'gaussian')
+            features(2:end,:) = exp(-opt.gamma*features(2:end,:));
+            params = [params ' -g ' num2str(opt.gamma)];
+        end
     end
-    
-	model.full_test_kernel = opt.full_test_kernel;
+end
 
-	model.train_set = train_set;
-	
-	if ~exist('svmtrain_inplace') || opt.no_inplace
-		model.svm = svmtrain(feature_class.', ...
-			double(features.'),params);
-	else
-		model.svm = svmtrain_inplace(feature_class, ...
-			single(features),params,ind_features);
-	end
+
+if opt.w
+    db_weights=calc_train_weights(db, train_set);
+    params = [params db_weights];
+end
+
+
+params=[params ' -b ' num2str(opt.b)];
+
+
+model.full_test_kernel = opt.full_test_kernel;
+
+model.train_set = train_set;
+
+if ~exist('svmtrain_inplace') || opt.no_inplace
+    model.svm = svmtrain(double(feature_class.'), ...
+        double(features.'),params);
+else
+    model.svm = svmtrain_inplace(feature_class, ...
+        single(features),params,ind_features);
+end
 end
 
 
@@ -133,26 +144,23 @@ function db_weights=calc_train_weights(db,train_set)
 % the weight of a particular class is the inverse of the total
 %number of its features.
 
-
 ind_objs={};
 ind_feats={};
 nb_feats=[];
 
 db_weights=[];
+
 for k=1:length(db.src.classes)
     ind_objs{k}=find([db.src.objects.class]==k);
     
-
     ind_feats{k}=[db.indices{ind_objs{k}}];
-   
-
-         mask2=ismember(ind_feats{k},[db.indices{train_set}]);
-         ind_maxp2=find(mask2>0);
-         ind_feats{k}=ind_feats{k}(ind_maxp2);
-         
-   
+    
+    mask2=ismember(ind_feats{k},[db.indices{train_set}]);
+    ind_maxp2=find(mask2>0);
+    ind_feats{k}=ind_feats{k}(ind_maxp2);
+    
     nb_feats(k)=numel(ind_feats{k});
-
+    
     db_weights = [db_weights ' -w' num2str(k) ' ' num2str(1/nb_feats(k))];
 end
 end
