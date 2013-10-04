c6176ab5620d561f7f6f5f9c94f3f5072f1716d4
laurentsifre <laurent.sifre@gmail.com>
2013-10-04 13:42:55 +0200

    pad_size

--- convolution/pad_size.m
+++ convolution/pad_size.m
@@ -1,4 +1,19 @@
+% PAD_SIZE Compute the optimal size for pading
 %
+% Usage
+%   sz_paded = PAD_SIZE(sz, min_margin, res)
+%
+% Input
+%   sz (int) : the size of the original signal
+%   min_margin (int) : the minimum margin for pading 
+%   res (int) : the downsampled resolution
+%
+% Output
+%   sz_paded (int) : the optimal size for pading
+%
+% Description
+%   the smallest multiple of 2^res that is
+%   larger than sz by at least 2*min_margin
 function sz_paded = pad_size(sz, min_margin, res)
-    sz_paded = 2^res*ceil((sz + 2*min_margin)/2^res);
+    sz_paded = 2^res * ceil( (sz + 2*min_margin)/2^res );
 end
