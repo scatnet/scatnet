6c4fe3d0624cda4b0e550d95ec070628d741a0a5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2013-10-04 12:18:54 +0200

    added log_scat back

---
+++ scatutils/log_scat.m
@@ -0,0 +1,49 @@
+% LOG_SCAT Calculate the logarithm of a scattering transform.
+% Usages
+%    S = log_scat(S)
+%
+%    S = log_scat(S,epsilon)
+%
+% Input
+%    S (cell): A scattering transform.
+%    epsilon (real): An small constant added to each component of S in
+%    order to avoid numerical issues : log(0) == -Inf. Default is 2^(-20).
+%
+% Output
+%    S: The scattering transform with the logarithm applied to each 
+%       coefficient.
+%
+% Description
+%   This function takes the logarithm of every signal component in a
+%   scattering transform, while preserving the network structure. A small
+%   additive constant is used to keep finite results. Note that this
+%   function may also take a single layer of the whole network as an
+%   argument, as it calls itself recursively layerwise.
+%
+% See also
+%   LOG_RENORM_SCAT
+
+function S = log_scat(S, epsilon)
+    % Default value for epsilon
+	if nargin < 2
+		epsilon = 2^(-20); % approximately 1e-6
+    end
+
+    % Logarithm of the whole scattering network
+	if iscell(S)
+		for m = 0:length(S)-1
+			S{m+1} = log_scat(S{m+1}, epsilon); % self-call
+        end
+		return;
+    end
+	
+    % Logarithm layer per layer
+	for p1 = 1:length(S.signal)
+		res = 0;
+		if isfield(S.meta,'resolution')
+			res = S.meta.resolution(p1);
+		end
+		sub_multiplier = 2^(res/2);
+		S.signal{p1} = log(abs(S.signal{p1})+epsilon*sub_multiplier);
+	end
+end
