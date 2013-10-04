d8bea73fa163b2b36436f01caa085c87ea9cf31e
Joakim Andén <joakim.anden@gmail.com>
2013-10-04 15:22:42 +0200

    update test for (un)pad_signal in 2d

--- test/convolution/test_pad_2d.m
+++ test/convolution/test_pad_2d.m
@@ -6,6 +6,12 @@ for i = 1:100
     margin = [100, 100];
     x_old = pad_mirror_2d_twosided(x, margin);
     x_new = pad_signal(x, size(x) + 2*margin, [], 1);
+	xp = unpad_signal(x_new, 0, size(x), 1);
+	assert(norm(xp-x)<1e-14);
+	x_new = pad_signal(x, size(x) + 2*margin, [], 0);
+	x_new = x_new(1:2:end,1:2:end);
+	xp = unpad_signal(x_new, 1, size(x), 0);
+	assert(norm(xp-x(1:2:end,1:2:end))<1e-14);
 end
 profile off;
 profile viewer;
