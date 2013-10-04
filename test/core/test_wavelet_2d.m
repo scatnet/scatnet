0c933c07e4cc0c3f41ccd620f30f138a4111c97d
laurentsifre <laurent.sifre@gmail.com>
2013-10-04 13:59:39 +0200

    test wav2d works

--- test/core/test_wavelet_2d.m
+++ test/core/test_wavelet_2d.m
@@ -1,4 +1,3 @@
-
 % load an image
 x = uiuc_sample;
 x = x(1:256, 1:256);
@@ -12,7 +11,7 @@ filters = morlet_filter_bank_2d(size(x), options);
 tic;
 [x_phi, x_psi] = wavelet_2d(x, filters, options);
 toc;
-
+%%
 % compute energy
 energy_x = sum(x(:).^2);
-energy_Wx = sum(x_phi.signal{1}(:).^2) + sum(cellfun(@(x)(sum(abs(x(:).^2))),x_psi.signal));
+energy_Wx = sum(x_phi(:).^2) + sum(cellfun(@(x)(sum(abs(x(:).^2))),x_psi));
