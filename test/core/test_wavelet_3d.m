2e2ec44acc6f7a1edecd316140876dd499610cee
laurentsifre <laurent.sifre@gmail.com>
2013-10-04 16:42:10 +0200

    fix wavelet 3d

--- test/core/test_wavelet_3d.m
+++ test/core/test_wavelet_3d.m
@@ -24,13 +24,13 @@ profile on;
 options.psi_mask = ones(1,numel(filters.psi.filter));
 options.psi_mask(1:8) = 0;
 y = reshape(abs(cell2mat({x_psi{1:8}})),[sz_in, 8]);
-[y_Phi, y_Psi] = wavelet_3d(y, filters, filters_rot, options);
+[y_Phi, y_Psi, meta_Phi, meta_Psi] = wavelet_3d(y, filters, filters_rot, options);
 profile off;
 profile viewer;
 plot_meta_layer(y_Psi.meta)
 
 %% check number of coefficient
-K = filters_rot.J;
+K = filters_rot.meta.J;
 J = filters.meta.J;
 L = filters.meta.L;
 
@@ -38,25 +38,25 @@ nb_phi_psi = K;
 nb_psi_psi = (J-1)*L;
 nb_phi_phi = K*(J-1)*L;
 nb_total = nb_phi_psi + nb_psi_psi + nb_phi_phi;
-assert( numel(y_Psi.signal) == nb_total);
+assert( numel(y_Psi) == nb_total);
 
 %% display cut of different signals
 close all;
 
 subplot(131);
-ycut = abs(y_Psi.signal{1});
+ycut = abs(y_Psi{1});
 ycut = squeeze(ycut(32,:,:));
 imagesc(ycut);
 title('y * phi psi');
 
 subplot(132);
-ycut = abs(y_Psi.signal{4});
+ycut = abs(y_Psi{4});
 ycut = squeeze(ycut(32,:,:));
 imagesc(ycut);
 title('y * psi phi');
 
 subplot(133);
-ycut = abs(y_Psi.signal{5});
+ycut = abs(y_Psi{5});
 ycut = squeeze(ycut(32,:,:));
 imagesc(ycut);
 title('y * psi psi');
