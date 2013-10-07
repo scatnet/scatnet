bc4889137d72eea2288fb0eff9851f7db18423e9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2013-10-07 17:17:33 +0200

    improved error handling and help header in filter_freq

--- filters/filter_freq.m
+++ filters/filter_freq.m
@@ -1,15 +1,23 @@
 % FILTER_FREQ Calculate center frequencies and bandwidths
 %
 % Usage
-%    [psi_xi, psi_bw, phi_bw] = FILTER_FREQ(filter_options)
+%    [psi_xi, psi_bw, phi_bw] = filter_freq(filt_opt)
 %
 % Input
-%    filter_options (struct): The parameters defining the filter bank.
+%    filt_opt (struct): The parameters defining the filter bank.
 %
 % Output
 %    psi_xi (numeric): The center frequencies of the wavelet filters.
 %    psi_bw (numeric): The bandwidths of the wavelet filters.
 %    phi_bw (numeric): The bandwidth of the lowpass filter.
+%
+% Description
+%    Called by WAVELET_1D and WAVELET_LAYER_1D, this function provides the
+%    center frequency and bandwidth of a mother wavelet, whose parameters
+%    are specified in filt_opt. It operates as a mere disjunction between
+%    SPLINE_FREQ_1D, and MORLET_FREQ_1D.
+% See also
+%   MORLET_FREQ_1D, SPLINE_FREQ_1D, WAVELET_1D, WAVELET_LAYER_1D
 
 
 function [psi_xi, psi_bw, phi_bw] = filter_freq(filter_options)
@@ -19,7 +27,6 @@ function [psi_xi, psi_bw, phi_bw] = filter_freq(filter_options)
 		strcmp(filter_options.filter_type,'gabor_1d')
 		[psi_xi,psi_bw,phi_bw] = morlet_freq_1d(filter_options);
 	else
-		error(sprintf('Unknown filter type ''%s''', ...
-			filter_options.filter_type));
+		error('Unknown filter type ''%s''', filter_options.filter_type);
 	end
 end
