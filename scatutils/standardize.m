c4b520c0b39e4b35f3a438bf365f62ffb61dd23c
laurentsifre <laurent.sifre@gmail.com>
2013-10-08 11:17:44 +0200

    substract mean

--- scatutils/standardize.m
+++ scatutils/standardize.m
@@ -1,15 +1,13 @@
-function db=standardize(db,epsilon)
-
-% Compute the standard deviation of each line of the matice db.features
-% and divide all the coefficients of this line by that value. At the end
-% each line has a variance of one.
-
-if nargin <2
-    epsilon=2^(-20)  % ~ 1e-6
+function db_std = standardize(db, epsilon)
     
+    % Compute the standard deviation of each line of the matice db.features
+    % and divide all the coefficients of this line by that value. At the end
+    % each line has a variance of one.
+    if (nargin<2)
+        epsilon = 1e-6;
+    end
+    db_std = db;
+    feat_mean = mean(db.features,2);
+    feat_std = std(db.features,0,2);
+    db_std.features = bsxfun(@rdivide, bsxfun(@minus,db.features,feat_mean), feat_std+epsilon);
 end
-
-db_sigma=std(db.features,[],2);
-db.features=db.features./(repmat(db_sigma,1,size(db.features,2))+epsilon);
-
-ebd
