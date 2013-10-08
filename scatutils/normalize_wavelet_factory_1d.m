56bc58fa2c162a7b54e5dd0d3a2e9f806263b6d3
laurentsifre <laurent.sifre@gmail.com>
2013-10-08 12:32:03 +0200

    finished to destroy the work of michel :)

--- scatutils/normalize_wavelet_factory_1d.m
+++ scatutils/normalize_wavelet_factory_1d.m
@@ -1,19 +1,23 @@
-function Wop = normalize_wavelet_factory_1d(N,filter_options,scat_options,epsilon)
-
-if nargin <4
-    epsilon = 2^(-20);
-end
-
-filters = filter_bank(N, filter_options);
-
-
-for m = 0:scat_options.M
-    filt_ind = min(numel(filters), m+1);
- 
-    Wop{m+1} = @(X)(renorm_wavelet_layer_1d(func_output(...
-        @wavelet_layer_1d,[1,2],X,filters{filt_ind},scat_options),epsilon));
-end
-
+function Wop = normalize_wavelet_factory_1d(N, filter_options, scat_options, epsilon)
+    
+    if nargin <4
+        epsilon = 2^(-20);
+    end
+    
+    filters = filter_bank(N, filter_options);
+    
+    
+    for m = 0:scat_options.M
+        filt_ind = min(numel(filters), m+1);
+        Wop{m+1} = @(X)(wavelet_renorm(X, m));
+    end
+    
+    function [S, Utilde] = wavelet_renorm(U, m)
+        filt_ind = min(numel(filters), m+1);
+        [S, U] = wavelet_layer_1d(U, filters{filt_ind}, scat_options);
+        Utilde = renorm_low_pass_layer_1d(S, U, epsilon);
+    end
+    
 end
 
 
--- scatutils/renorm_parent_2d.m
+++ scatutils/renorm_parent_2d.m
@@ -1,12 +1,12 @@
 function Sx_rn = renorm_parent_2d(Sx)
-Sx_rn = Sx;
-
-parent = @(p)(find(Sx{2}.meta.j(1,:) == Sx{3}.meta.j(1,p) &...
-    Sx{2}.meta.theta(1,:) == Sx{3}.meta.theta(1,p) & ...
-    Sx{2}.meta.q(1,:) == Sx{3}.meta.q(1,p)));
-
-for p = 1:numel(Sx{3}.signal)
-    Sx_rn{3}.signal{p} = Sx_rn{3}.signal{p}./Sx{2}.signal{parent(p)};
-end
-
+    Sx_rn = Sx;
+    
+    parent = @(p)(find(Sx{2}.meta.j(1,:) == Sx{3}.meta.j(1,p) &...
+        Sx{2}.meta.theta(1,:) == Sx{3}.meta.theta(1,p) & ...
+        Sx{2}.meta.q(1,:) == Sx{3}.meta.q(1,p)));
+    
+    for p = 1:numel(Sx{3}.signal)
+        Sx_rn{3}.signal{p} = Sx_rn{3}.signal{p}./Sx{2}.signal{parent(p)};
+    end
+    
 end
