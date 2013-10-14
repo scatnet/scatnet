f2e195f37e8fb6e49fb7e61632d17856800a4faf
laurentsifre <laurent.sifre@gmail.com>
2013-10-13 18:20:24 -0700

    minor fix in doc wav fact 3d

--- core/wavelet_factory_3d_pyramid.m
+++ core/wavelet_factory_3d_pyramid.m
@@ -1,14 +1,14 @@
 % WAVELET_FACTORY_3D_PYRAMID Build roto-translation wavelet operators
 %
 % Usage
-%   [Wop, filters, filters_rot] = WAVELET_FACTORY_3D_PYRAMID(size_in, filt_opt, filt_rot_opt, scat_opt)
+%   [Wop, filters, filters_rot] = WAVELET_FACTORY_3D_PYRAMID(filt_opt, filt_rot_opt, scat_opt)
 %
 % Input
-%    filt_opt (struct): the filter options, same as for MORLET_FILTER_BANK_2D_PYRAMID 
-%    filt_rot_opt (struct): the filter options for the 
-%		filters along angular parameter, same as for MORLET_FILTER_BANK_1D 
+%    filt_opt (struct): the filter options, same as for MORLET_FILTER_BANK_2D_PYRAMID
+%    filt_rot_opt (struct): the filter options for the
+%		filters along angular parameter, same as for MORLET_FILTER_BANK_1D
 %	 scat_opt (struct): the scattering and wavelet options, same as
-%		WAVELET_LAYER_3D_PYRAMID 
+%		WAVELET_LAYER_3D_PYRAMID
 %
 % Output
 %    Wop: A cell array of wavelet transforms needed for the scattering trans-
@@ -16,7 +16,7 @@
 %    filters: A cell array of the filters used in defining the wavelets.
 %
 % Description
-%   This function builds the wavelet operators used to compute the 
+%   This function builds the wavelet operators used to compute the
 %   roto-translation scattering. The first operator is a 2d wavelet
 %   transform obtained with WAVELET_LAYER_2D_PYRAMID. The second operator and third
 %   operators are roto-translation wavelet transform obtained with
@@ -30,8 +30,8 @@
 %   WAVELET_3D_LAYER_PYRAMID
 %
 
-function [Wop, filters, filters_rot] = wavelet_factory_3d_pyramid(filt_opt, filt_rot_opt, scat_opt)	
-	if (nargin < 3)
+function [Wop, filters, filters_rot] = wavelet_factory_3d_pyramid(filt_opt, filt_rot_opt, scat_opt)
+    if (nargin < 3)
         scat_opt = struct();
     end
     if (nargin < 2)
@@ -40,28 +40,28 @@ function [Wop, filters, filters_rot] = wavelet_factory_3d_pyramid(filt_opt, filt
     if (nargin < 1)
         filt_opt = struct();
     end
-	scat_opt = fill_struct(scat_opt, 'M', 2);
+    scat_opt = fill_struct(scat_opt, 'M', 2);
     wavelet_opt = rmfield(scat_opt, 'M');
-	
-	precision = getoptions(scat_opt, 'precision', 'single');
-	% filters :
-	filters = morlet_filter_bank_2d_pyramid(filt_opt);
-	
-	% filters along angular variable
-	sz = filters.meta.L * 2; % L orientations between 0 and pi
-	% periodic convolutions along angles
-	filt_rot_opt.boundary = 'per';
-	filt_rot_opt.filter_format = 'fourier_multires';
-	filt_rot_opt.J = 3;
-	filt_rot_opt.P = 0;
-	filt_rot_opt.Q = 1;
-	filt_rot_opt.precision = precision;
-	filters_rot = morlet_filter_bank_1d(sz, filt_rot_opt);
-	
-	% first layer : usual 2d wavelet transform
-	Wop{1} = @(x)(wavelet_layer_2d_pyramid(x, filters, wavelet_opt));
-	Wop{2} = @(x)(wavelet_layer_3d_pyramid(x, filters, filters_rot, wavelet_opt));
-	Wop{3} = @(x)(wavelet_layer_3d_pyramid(x, filters, filters_rot, wavelet_opt));
-	
+    
+    precision = getoptions(scat_opt, 'precision', 'single');
+    % filters :
+    filters = morlet_filter_bank_2d_pyramid(filt_opt);
+    
+    % filters along angular variable
+    sz = filters.meta.L * 2; % L orientations between 0 and pi
+    % periodic convolutions along angles
+    filt_rot_opt.boundary = 'per';
+    filt_rot_opt.filter_format = 'fourier_multires';
+    filt_rot_opt.J = 3;
+    filt_rot_opt.P = 0;
+    filt_rot_opt.Q = 1;
+    filt_rot_opt.precision = precision;
+    filters_rot = morlet_filter_bank_1d(sz, filt_rot_opt);
+    
+    % first layer : usual 2d wavelet transform
+    Wop{1} = @(x)(wavelet_layer_2d_pyramid(x, filters, wavelet_opt));
+    Wop{2} = @(x)(wavelet_layer_3d_pyramid(x, filters, filters_rot, wavelet_opt));
+    Wop{3} = @(x)(wavelet_layer_3d_pyramid(x, filters, filters_rot, wavelet_opt));
+    
 end
 
