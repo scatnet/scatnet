2de29e51e7a635cb12dd748a172a7a02b69a7e82
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2013-10-15 14:21:53 +0200

    improved doc

--- demo/display/demo_image_scat.m
+++ demo/display/demo_image_scat.m
@@ -15,12 +15,11 @@ image_scat(S);
 % They correspond to the resulting scattering coefficients of each paths
 % for each layer and each rotation. The legend indicates the path and the resolution.
 % These filters are normalized but the normalization can be removed by
-% modifying the option of image_scat. The legend too can be removed using
-% too an option.
+% modifying the option of image_scat. The caption can also be removed with
+% a specific option.
 %
-% The part of the image where the legend is, has some transparent blue to 
-% help reading the coefficients, this is not a bug but it is here to be 
-% convenient for the readers.
+% To improve legibility, the image background behind the caption is
+% artificially enlightened.
 %
 % On the Figure 2 for instance, there is 2 different j. The first one
 % corresponds to the scale of the used wavelets whereas the second one
