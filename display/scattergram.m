fea5210848b9d18783187f338c62d7f4f1c40409
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2013-10-15 14:19:10 +0200

    updated doc and renamed scat_img to scattergram_layer in scattergram

--- display/scattergram.m
+++ display/scattergram.m
@@ -23,34 +23,20 @@
 %    prefix can be given as input. The images generated are output as a cell
 %    array of tables in img.
 %
+%    This function calls SCATTERGRAM_LAYER iteratively, in order to get the
+%    scattering coefficients in a two-dimensional array, indexed by scale
+%    and time.
+%
 % See also 
-%    DISPLAY_SLICE
+%    DISPLAY_SLICE, SCATTERGRAM_LAYER
 
 function img = scattergram(varargin)
 	ndisp = nargin/2;
 	for n = 1:ndisp
-		img{n} = scat_img(varargin{(n-1)*2+1},varargin{(n-1)*2+2});
-		subplot(ndisp,1,n);
+		img{n} = ...
+            scattergram_layer(varargin{(n-1)*2+1},varargin{(n-1)*2+2});
+        subplot(ndisp,1,n);
 		imagesc(img{n});
     end
     colormap gray; % avoids default jet colormap
-end
-
-function img = scat_img(X,j)
-	if all(size(j)==[0 0])
-		j = zeros(0,1);
-	end
-	
-	nbTimePt = max(cellfun(@(x)(size(x,1)), X.signal));
-	
-	max_jm = max(X.meta.j(end,:)+1);
-	
-	img = -Inf*ones(max_jm, nbTimePt);
-
-	ind = find(all(bsxfun(@eq,X.meta.j(1:end-1,:),j),1));
-	
-	signal = cellfun(@(x)(interpft(x,nbTimePt)), ...
-		X.signal(ind),'UniformOutput',false);
-	
-	img(X.meta.j(end,ind)+1,:) = [signal{:}].';
 end
