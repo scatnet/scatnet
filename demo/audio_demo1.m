fb35c48e099c2d834cbad4e055f5898ec5e8a81e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2013-10-16 14:13:21 +0200

    updated the audio demo

--- demo/audio_demo1.m
+++ demo/audio_demo1.m
@@ -4,7 +4,8 @@ load handel;
 y = y(1:N);
 
 % Set up default filter bank with averaging scale of 4096 samples.
-filt_opt = default_filter_options('audio', 4096);
+T = 4096;
+filt_opt = default_filter_options('audio', T);
 
 % Only compute zeroth-, first- and second-order scattering.
 scat_opt.M = 2;
@@ -25,19 +26,55 @@ for m = 1:2
 	xlim([1 5*N/8]);
 end
 
+%%
 % Compute the scattering coefficients of y.
 S = scat(y, Wop);
-
+%%
 % Display first-order coefficients, as a function of time and scale j1, and
 % second-order coefficients for a fixed j1, as a function of time and second
 % scale j2.
+img{1} = scattergram_layer(S{1+1},[]);
 j1 = 23;
-scattergram(S{2},[],S{3},j1);
+img{2} = scattergram_layer(S{1+2},j1);
+time = (1:size(img{1},2))/size(img{1},2) * N/Fs;
 
+figure;
+title('Scattergram of first and second-order coefficients');
+subplot(2,1,1);
+imagesc(time,1:size(img{1},1),img{1});
+xlabel('Time (s)');
+ylabel('Scale number j1');
+colorbar;
+subplot(2,1,2);
+imagesc(time,1:size(img{2},1),img{2});
+xlabel('Time (s)');
+ylabel('Scale number j1');
+colorbar;
+
+% Set the colormap to inverted grayscale for potential ink saving.
+colormap(1-colormap(gray));
+
+%%
 % Renormalize second order by dividing it by the first order and compute the
 % logarithm of the coefficients.
-S = renorm_scat(S);
-S = log_scat(S);
+logrenorm_S = log_scat(renorm_scat(S));
 
 % Display the transformed coefficients.
-scattergram(S{2},[],S{3},j1);
+img_logrenorm{1} = scattergram_layer(logrenorm_S{1+1},[]);
+j1 = 23; % arbitrary choice
+img_logrenorm{2} = scattergram_layer(logrenorm_S{1+2},j1);
+time = (1:size(img_logrenorm{1},2))/size(img_logrenorm{1},2) * N/Fs;
+
+figure;
+title('Scattergram of log_renormalized coefficients');
+subplot(2,1,1);
+imagesc(time,1:size(img_logrenorm{1},1),img_logrenorm{1});
+xlabel('Time (s)');
+ylabel('Scale number j1');
+colorbar;
+subplot(2,1,2);
+imagesc(time,1:size(img_logrenorm{2},1),img_logrenorm{2});
+xlabel('Time (s)');
+ylabel('Scale number j2');
+colorbar;
+colormap(1-colormap(gray));
