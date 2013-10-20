cba5234dd342aec49af11dbf8c91ad5f4802fc99
kapokmi <michelkapoko@gmail.com>
2013-10-20 12:19:19 +0200

    minor change

--- classification/classif_recog.m
+++ classification/classif_recog.m
@@ -1,22 +1,43 @@
-function [rg_mean,recog_rate] = classif_recog(labels,test_set,truth)
+function [rg_mean,recog_rate]=classif_recog(labels,test_set,truth)
+
+% Usage
+%    [rr_mean,recog_rate] =  classif_recog(labels,test_set,truth)
+%
+% Input
+%    labels (int): The labels attributed to the testing instances.
+%    test_set (int): The object indices of the testing instances.
+%    truth: the actual labels of the testing instances
+%          
+% Output
+%    rr_mean (real): The mean recognition rate
+%    recog_rate(real): array containing the individual recognition rates of
+% each class.
+%
+% Description
+%    This function computes the average recognition rate of each class.
+% It is a widely used measure of the performance of the classifier when the
+% data set is highly unbalanced.
+%
+% See also
+%    SVM_TEST, CLASSIF_ERR, CLASSIF_RECOG
 
     if isstruct(truth)
-        src = truth;
+        src=truth;
         truth = [src.objects.class];
     end
 
     % Normally, the test_set contains samples of all classes
-    recog_rate = zeros(1,max(truth));
-    gdTruth = truth(:,test_set);
+    recog_rate=zeros(1,max(truth));
+    gdTruth=truth(:,test_set);
 
 
     for k=1:max(truth)
 
-        mask=k == gdTruth;
-        good_elts = find(labels==k & mask);
+        mask=k==gdTruth;
+        good_elts=find(labels==k & mask);
 
-        mask1 = numel(find(mask==1));
-        recog_rate(k) = numel(good_elts)/mask1;
+        mask1=numel(find(mask==1));
+        recog_rate(k)=numel(good_elts)/mask1;
 
     end
 
