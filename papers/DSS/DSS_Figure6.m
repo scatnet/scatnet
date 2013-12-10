5e8e66855f47e28c8aedcd08e4d9f52d905018a2
janden <joakim.anden@gmail.com>
2013-12-11 00:00:28 +0100

    fix up DSS_Figure6/7 for publication

--- papers/DSS/DSS_Figure6.m
+++ papers/DSS/DSS_Figure6.m
@@ -9,7 +9,7 @@ filt_opt.B = [4 4];
 filt_opt.Q = 4*filt_opt.B; 
 filt_opt.J = T_to_J([256 32768],filt_opt);
 
-scat_opt.oversampling = 8;
+scat_opt.oversampling = 3;
 scat_opt.M = 2;
 
 Wop = wavelet_factory_1d(length(x), filt_opt, scat_opt);
@@ -18,16 +18,28 @@ Wop = wavelet_factory_1d(length(x), filt_opt, scat_opt);
 [S, U] = scat(x, Wop);
 
 % Renormalize coefficients.
-epsilon = 1e-2;
+epsilon = 1e-3;
 S = renorm_scat(S, epsilon);
-S = renorm_1st_phi(S, U, Wop, epsilon);
+S = renorm_1st(S, x, Wop{2}, epsilon);
+
+S = resample_scat(S, 8, false);
+U = resample_scat(U, 6, false);
 
 % Compute the logarithm.
-S = log_scat(S,1e-3);
-U = log_scat(U,1e-3);
+S = log_scat(S, 1e-3);
+U = log_scat(U, 1e-3);
 
 % Display scalogram U{2}, first-order coefficients S{2} and second-order
 % coefficients S{3} for j1 = 58.
 figure(6);
+clf;
+set(gcf,'Units','pixels','Position',[200 200 560 420]);
 scattergram(U{2}, [], S{2}, [], S{3}, 58);
+subplot(3,1,1);
+set(gca,'XTick',[],'YTick',[]);
+subplot(3,1,2);
+set(gca,'XTick',[],'YTick',[]);
+subplot(3,1,3);
+set(gca,'XTick',[],'YTick',[]);
+colormap(jet);
 
--- papers/DSS/DSS_Figure7.m
+++ papers/DSS/DSS_Figure7.m
@@ -9,8 +9,10 @@ filt_opt.B = [8 4];
 filt_opt.Q = 8*filt_opt.B;
 filt_opt.J = T_to_J([256 4096],filt_opt);
 
-scat_opt.oversampling = 6;
+scat_opt.oversampling = 3;
 scat_opt.M = 2;
+
+scat_opt.path_margin = 1;
 
 Wop = wavelet_factory_1d(length(x), filt_opt, scat_opt);
 
@@ -22,6 +24,9 @@ epsilon = 1e-3;
 S = renorm_scat(S, epsilon);
 S = renorm_1st_phi(S, U, Wop, epsilon);
 
+S = resample_scat(S, 8);
+U = resample_scat(U, 8);
+
 % Compute the logarithm.
 S = log_scat(S,1e-3);
 U = log_scat(U,1e-3);
@@ -29,4 +34,16 @@ U = log_scat(U,1e-3);
 % Display scalogram U{2}, first-order coefficients S{2} and second-order
 % coefficients S{3} for j1 = 110.
 figure(7);
+colormap(jet);
+clf;
+set(gcf,'Units','pixels','Position',[200 200 560 420]);
 scattergram(U{2}, [], S{2}, [], S{3}, 110);
+subplot(3,1,1);
+set(gca,'XTick',[],'YTick',[]);
+subplot(3,1,2);
+set(gca,'XTick',[],'YTick',[]);
+subplot(3,1,3);
+set(gca,'XTick',[],'YTick',[]);
+ylim([150.5 299.5]);
+colormap(jet);
+
