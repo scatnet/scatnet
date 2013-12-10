85637ea578384b55fa4fd677d2f3290a540ab658
janden <joakim.anden@gmail.com>
2013-12-10 04:31:27 +0100

    fix up DSS_Figure3 for publication

--- papers/DSS/DSS_Figure3.m
+++ papers/DSS/DSS_Figure3.m
@@ -12,21 +12,17 @@ filters = filter_bank(N, filt_opt);
 % Extract wavelets psi in Fourier domain.
 psi_f = cellfun(@realize_filter, filters{1}.psi.filter, 'UniformOutput', 0);
 psi_f = [psi_f{:}];
+psi_f = bsxfun(@times, psi_f, 1./max(psi_f,[],1));
 
 % Extract lowpass phi in Fourier domain.
 phi_f = realize_filter(filters{1}.phi.filter);
 
 % Set up figure dimensions.
-fig_width = 3.5;
-fig_height = 0.6;
 figure(3);
 clf;
-set(gcf, 'PaperSize', [fig_width fig_height]);
-set(gcf, 'PaperPosition', [3 3 fig_width fig_height]);
-set(gcf, 'Units', 'inches');
-set(gcf, 'Position', [3 3 fig_width fig_height]);
+set(gcf, 'Units', 'inches', 'Position', [3 3 3.5 0.6]);
 axes('Units', 'inches', ...
-	'Position', [0.05 0.05 fig_width-0.1 fig_height-0.1]);
+	'Position', [0.05 0.05 3.5-0.1 0.6-0.1]);
 
 % Plot psis and phi.
 figure(3);
