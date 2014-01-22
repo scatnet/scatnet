b1572c2460721800cb0c990aea23a6bc28276af3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2014-01-22 15:00:29 +0100

    Replaced variables by standard notations

--- core/joint_tf_wavelet_factory_1d.m
+++ core/joint_tf_wavelet_factory_1d.m
@@ -1,21 +1,23 @@
-function wavelet = joint_tf_wavelet_factory_1d(N, time_filter_options, freq_filter_options, scat_options)
-	M = scat_options.M;
+function Wop = ...
+    joint_tf_wavelet_factory_1d(N,time_filt_opt,freq_filt_opt,scat_opt)
 
-	time_filters = filter_bank(N, time_filter_options);
-	
-	N_freq = 2^ceil(log2(numel(time_filters{1}.psi.filter)));
-	
-	freq_filters = filter_bank(N_freq, freq_filter_options);
-	
-	scat_options1 = scat_options;
-	scat_options1.phi_renormalize = 0;
-	wavelet{1} = @(X)(wavelet_layer_1d(X, time_filters{1}, scat_options1));
+	time_filters = filter_bank(N, time_filt_opt);
+	N_freq = 2^nextpow2(numel(time_filters{1}.psi.filter));
+	freq_filters = filter_bank(N_freq, freq_filt_opt);
+	scat_opt1 = scat_opt;
+	scat_opt1.phi_renormalize = 0;
+    
+    Wop = cell(1,scat_opt.M+1);
+	Wop{1} = @(X)(wavelet_layer_1d(X, time_filters{1}, scat_opt1));
 	
 	for m = 1:M
 		time_filt_ind = min(numel(time_filters), m+1);
 		freq_filt_ind = min(numel(freq_filters), m+1);
-		wavelet{m+1} = @(X)(joint_tf_wavelet_layer_1d(X, {freq_filters{freq_filt_ind}, time_filters{time_filt_ind}}, scat_options));
-	end
-
+		Wop{1+m} = @(X)( ...
+            joint_tf_wavelet_layer_1d(X, ...
+            {freq_filters{freq_filt_ind},time_filters{time_filt_ind}}, ...
+            scat_opt));
+    end
 end
+
 
