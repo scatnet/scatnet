a89102d619a130c0186686efe73b0d61368de31e
Joakim Andén <janden@math.princeton.edu>
2015-03-29 15:02:45 -0400

    make DSS_Table1 deal with cell arrays (easier to drop in a parfor)

--- papers/DSS/DSS_Table1.m
+++ papers/DSS/DSS_Table1.m
@@ -3,6 +3,9 @@ opt.filter_type = {'morlet_1d','spline_1d'};
 opt.B = [8 1];
 opt.Q = [super*8 1];
 opt.filter_format = 'fourier_truncated';
+% need periodic boundary conditions because the symmetrization/desymmetrization
+% does not preserve energy, especially at large T
+opt.boundary = 'per';
 M = 3;
 
 Js = [9:2:15];
@@ -15,6 +18,7 @@ rs = RandStream.create('mt19937ar','Seed',floor(pi*1e9));
 files = files(rs.randperm(length(files)));
 
 scat_opt.M = M;
+scat_opt.path_margin = 1;
 scat_opt.oversampling = 2;
 
 for l = 1:length(Js)
@@ -25,10 +29,11 @@ for l = 1:length(Js)
 	[Wop{l},filters{l}] = wavelet_factory_1d(N, opts{l}, scat_opt); 
 end
 
-E = zeros(length(Js),M+1,length(files));
+E = cell(1,length(files));
 
 for k = 1:length(files)
 	fprintf('%s\n',files{k});
+	tt = tic;
 	x = data_read(files{k});
 	x = x(1:min(N,length(x)));
 	x = [x; zeros(N-length(x),1)];	
@@ -40,17 +45,19 @@ for k = 1:length(files)
 		t = squeeze(t);
 		for m = 0:max(meta.order)
 			t1 = t(meta.order==m,:);
-			E(l,m+1,k) = sum(abs(t1(:)).^2);
+			E{k}(l,m+1) = sum(abs(t1(:)).^2);
 		end
 	end
-	mean(E(:,:,1:k),3)
+	fprintf('%.2f s\n',toc(tt));
 end
+
+E = reshape(cell2mat(E), [size(E{1}) length(E)]);
+
+E_mean = mean(E,3);
 
 % At the end, we should have
 %	0.0000    0.9453    0.0484    0.0023
 %	0.0000    0.6800    0.2900    0.0191
 %	0.0000    0.3486    0.5325    0.1156
 %	0.0000    0.2772    0.5607    0.2471
-
-
 
