87b1cbd4e89e9bd3765e6b07a9b843de68a820b3
Joakim Andén <janden@math.princeton.edu>
2016-01-31 18:00:53 -0500

    Formatting of prepare_database

--- classification/prepare_database.m
+++ classification/prepare_database.m
@@ -1,17 +1,18 @@
 % PREPARE_DATABASE Calculates the features from objects in a source
 %
 % Usage
-%    database = PREPARE_DATABASE(src, feature_fun, options)
+%    database = PREPARE_DATABASE(src, feature_fun, options);
 %
 % Input
 %    src (struct): The source specifying the objects.
 %    feature_fun (cell): The feature functions applied to each object.
 %    options (struct): Options for calculating the features:
-%       options.feature_sampling (int): specifies how to sample the feature 
+%       options.feature_sampling (int): specifies how to sample the feature
 %           vectors in time/space (default 1).
-%       options.file_normalize (int): The normalization of each file before 
-%          being given to feature_fun. Can be empty, 1, 2, or Inf (default []).
-%       options.parallel (boolean): If true, tries to use the Distributed 
+%       options.file_normalize (int): The normalization of each file before
+%          being given to feature_fun. Can be empty, 1, 2, or Inf (default
+%          []).
+%       options.parallel (boolean): If true, tries to use the Distributed
 %          Computing Toolbox to speed up calculation (default true).
 %       Other options are listed in the help for the FEATURE_WRAPPER function.
 % Output
@@ -37,8 +38,9 @@ function db = prepare_database(src,feature_fun,opt)
 	precision = 'double';
 	
 	if opt.parallel
-		% parfor loop - note that the contents are the same as the serial loop, but
-		% MATLAB doesn't seem to offer an easy way of deduplicating the code.
+		% parfor loop - note that the contents are the same as the serial
+		% loop, but MATLAB doesn't seem to offer an easy way of deduplicating
+		% the code.
 
 		% Loop through all the files in the source
 		parfor k = 1:length(src.files)
@@ -65,16 +67,18 @@ function db = prepare_database(src,feature_fun,opt)
 				end
 			end
 		
-			% Due to parallelization constraints, each file stores its feature vectors 
-			% in one index of the features cell array. The corresponding object indices
-			% are stored in the obj_ind cell array.
+			% Due to parallelization constraints, each file stores its feature
+			% vectors in one index of the features cell array. The
+			% corresponding object indices are stored in the obj_ind cell
+			% array.
 			features{k} = cell(1,length(file_objects));
 			obj_ind{k} = file_objects;
 			
-			% Apply all the feature functions to the objects contained in x. The output
-			% buf is a PxNxK vector, where P corresponds to number of feature coeffi-
-			% cients, N corresponds to the number of features - the number of "windows"
-			% - in each object, and K corresponds to the number of objects.
+			% Apply all the feature functions to the objects contained in x.
+			% The output buf is a PxNxK vector, where P corresponds to number
+			% of feature coeffi- cients, N corresponds to the number of
+			% features - the number of "windows" - in each object, and K
+			% corresponds to the number of objects.
 			buf = apply_features(x,src.objects(file_objects),feature_fun,opt);
 			
 			% Subsample among the features as needed.
@@ -128,10 +132,11 @@ function db = prepare_database(src,feature_fun,opt)
 	% Identify the files for which objects have been computed.
 	nonempty = find(~cellfun(@isempty,features));
 
-	% Determine the size of the features array. Each row corresponds to a feature 
-	% coefficient, each column corresponds to a feature vector.
+	% Determine the size of the features array. Each row corresponds to a
+	% feature coefficient, each column corresponds to a feature vector.
 	rows = size(features{nonempty(1)}{1},1);
-	cols = sum(cellfun(@(x)(sum(cellfun(@(x)(size(x,2)),x))),features(nonempty)));
+	cols = sum(cellfun(@(x)(sum(cellfun(@(x)(size(x,2)),x))), ...
+		features(nonempty)));
 	precision = class(features{nonempty(1)}{1});
 	
 	db.src = src;
@@ -163,12 +168,12 @@ function out = apply_features(x,objects,feature_fun,opt)
 	out = {};
 	for k = 1:length(feature_fun)
 		if nargin(feature_fun{k}) == 2
-			% If only two inputs are given, the feature function will handle the cut
-			% ting up of x into objects itself.
+			% If only two inputs are given, the feature function will handle
+			% the cut ting up of x into objects itself.
 			out{k,1} = feature_fun{k}(x,objects);
 		elseif nargin(feature_fun{k}) == 1
-			% If only one input is given, the feature function relies on us to cut up
-			% the input x into objects and feed them to it.
+			% If only one input is given, the feature function relies on us to
+			% cut up the input x into objects and feed them to it.
 			out{k,1} = feature_wrapper(x,objects,feature_fun{k},opt);
 		else
 			error('Invalid number of inputs for feature function!')
@@ -178,9 +183,10 @@ function out = apply_features(x,objects,feature_fun,opt)
 	if length(feature_fun) == 1
 		out = out{1};
 	else
-		% Concatenate along the first dimension - the feature dimension - of each
-		% element.
+		% Concatenate along the first dimension - the feature dimension - of
+		% each element.
 		out = cellfun(@(x)(permute(x,[2 1 3])),out,'UniformOutput',false);
 		out = permute([out{:}],[2 1 3]);
 	end
 end
+
